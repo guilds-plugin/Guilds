fe13b84ac5efbf74a4ac4f06e66d41d9d773752b
darbyjack <amheckey@bluevalleyk12.net>
2017-06-05 22:27:11 -0500

    Aqua it is!

--- src/main/java/me/bramhaag/guilds/commands/CommandList.java
+++ src/main/java/me/bramhaag/guilds/commands/CommandList.java
@@ -26,7 +26,7 @@ public class CommandList extends CommandBase {
                 .map(Guild::getName)
                 .collect(Collectors.joining("\n"));
         Message.sendMessage(player, Message.COMMAND_LIST_FORMAT);
-        player.sendMessage(ChatColor.RED + guilds);
+        player.sendMessage(ChatColor.AQUA + guilds);
 
     }
 }
