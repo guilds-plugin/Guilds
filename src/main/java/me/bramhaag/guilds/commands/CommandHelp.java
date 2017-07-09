7318c07746734ff582872b0cfe774d3c20b54412
darbyjack <amheckey@bluevalleyk12.net>
2017-07-09 16:04:55 -0500

    Users can now change plugin descriptions

--- src/main/java/me/bramhaag/guilds/commands/CommandHelp.java
+++ src/main/java/me/bramhaag/guilds/commands/CommandHelp.java
@@ -3,6 +3,7 @@ package me.bramhaag.guilds.commands;
 import me.bramhaag.guilds.Main;
 import me.bramhaag.guilds.commands.base.CommandBase;
 import me.bramhaag.guilds.message.Message;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
 public class CommandHelp extends CommandBase {
@@ -15,39 +16,43 @@ public class CommandHelp extends CommandBase {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (Main.getInstance().getConfig().getBoolean("description.patch")) {
+            Bukkit.dispatchCommand(sender, "help guilds");
 
-        boolean nextPage = true;
+        } else {
+            boolean nextPage = true;
 
-        int page = 1;
+            int page = 1;
 
-        if (args.length > 0) {
-            try {
-                page = Integer.valueOf(args[0]);
-            } catch (NumberFormatException ex) {
-                Message.sendMessage(sender, Message.COMMAND_ERROR_INVALID_NUMBER.replace("{input}", args[0]));
+            if (args.length > 0) {
+                try {
+                    page = Integer.valueOf(args[0]);
+                } catch (NumberFormatException ex) {
+                    Message.sendMessage(sender, Message.COMMAND_ERROR_INVALID_NUMBER.replace("{input}", args[0]));
+                }
             }
-        }
 
-        for (int i = 0; i < MAX_PAGE_SIZE; i++) {
+            for (int i = 0; i < MAX_PAGE_SIZE; i++) {
 
-            int index = ((page - 1) * 6) + i;
-            if (index > Main.getInstance().getCommandHandler().getCommands().size() - 1) {
-                nextPage = false;
-                if (i == 0) {
-                    Message.sendMessage(sender, Message.COMMAND_HELP_INVALID_PAGE);
+                int index = ((page - 1) * 6) + i;
+                if (index > Main.getInstance().getCommandHandler().getCommands().size() - 1) {
+                    nextPage = false;
+                    if (i == 0) {
+                        Message.sendMessage(sender, Message.COMMAND_HELP_INVALID_PAGE);
+                    }
+
+                    break;
                 }
 
-                break;
+                CommandBase command = Main.getInstance().getCommandHandler().getCommands().get(index);
+
+                Message.sendMessage(sender, Message.COMMAND_HELP_MESSAGE.replace("{command}", command.getName(), "{arguments}", command.getArguments(), "{description}", command.getDescription()));
+
             }
 
-            CommandBase command = Main.getInstance().getCommandHandler().getCommands().get(index);
-
-            Message.sendMessage(sender, Message.COMMAND_HELP_MESSAGE.replace("{command}", command.getName(), "{arguments}", command.getArguments(), "{description}", command.getDescription()));
-
-        }
-
-        if (nextPage) {
-            Message.sendMessage(sender, Message.COMMAND_HELP_NEXT_PAGE.replace("{next-page}", String.valueOf((page + 1))));
+            if (nextPage) {
+                Message.sendMessage(sender, Message.COMMAND_HELP_NEXT_PAGE.replace("{next-page}", String.valueOf((page + 1))));
+            }
         }
     }
 }
