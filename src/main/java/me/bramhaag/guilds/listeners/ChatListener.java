eeda2045564b86efc8edcea2eda3766086b0c697
darbyjack <amheckey@bluevalleyk12.net>
2017-07-09 02:06:11 -0500

    Hotfix for EssentialsChat Placeholders

--- src/main/java/me/bramhaag/guilds/listeners/ChatListener.java
+++ src/main/java/me/bramhaag/guilds/listeners/ChatListener.java
@@ -16,11 +16,19 @@ public class ChatListener implements Listener {
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         Guild guild = Guild.getGuild(player.getUniqueId());
-        event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD}", guild.getName()));
-        event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_PREFIX}", guild.getPrefix()));
-        event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MASTER}", Bukkit.getOfflinePlayer(guild.getGuildMaster().getUniqueId()).getName()));
-        event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBER_COUNT}", getGuildMemberCount(event.getPlayer())));
-        event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBERS_ONLINE}", getGuildMembersOnline(event.getPlayer())));
+        if (guild == null) {
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD}", ""));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_PREFIX}", ""));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MASTER}", ""));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBER_COUNT}", ""));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBERS_ONLINE}", ""));
+        } else {
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD}", guild.getName()));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_PREFIX}", guild.getPrefix()));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MASTER}", Bukkit.getOfflinePlayer(guild.getGuildMaster().getUniqueId()).getName()));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBER_COUNT}", getGuildMemberCount(event.getPlayer())));
+            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBERS_ONLINE}", getGuildMembersOnline(event.getPlayer())));
+        }
     }
 }
 
