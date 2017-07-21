92cbcfa005a4b92ffbf834e3dd3aebc30cda88ce
darbyjack <admin@glaremasters.me>
2017-07-21 12:03:13 -0500

    Finished up Guild Vaults

--- src/main/java/me/glaremasters/guilds/Main.java
+++ src/main/java/me/glaremasters/guilds/Main.java
@@ -215,7 +215,7 @@ public class Main extends JavaPlugin {
                 .scheduleAsyncRepeatingTask(this, this::sendUpdate, 0L, 2000L); //5 minutes
         }
 
-        if (!getConfig().isSet("version") || getConfig().getInt("version") == 1) {
+        if (!getConfig().isSet("version") || getConfig().getInt("version") == 1 || getConfig().getInt("version") == 2 ) {
             File oldfile = new File(this.getDataFolder(), "config.yml");
             File newfile = new File(this.getDataFolder(), "config-old.yml");
             oldfile.renameTo(newfile);
