d69a727c094736dbbee25021d1743b11526100c1
Redrield <redrield@gmail.com>
2017-08-12 18:53:37 -0400

    Not sure why intellij didn't catch this. Commented out MessageKeys

--- src/main/java/me/glaremasters/guilds/util/GuildMessageKeys.java
+++ src/main/java/me/glaremasters/guilds/util/GuildMessageKeys.java
@@ -1,12 +1,12 @@
-package me.glaremasters.guilds.util;
-
-import co.aikar.locales.MessageKey;
-
-public class GuildMessageKeys {
-
-    public static final MessageKey INVALID_GUILD_NAME = MessageKey.of("guilds.invalid-guild-name");
-    public static final MessageKey ALREADY_A_MEMBER = MessageKey.of("guilds.invite.already-a-member");
-    public static final MessageKey NOT_INVITED = MessageKey.of("guilds.invite.not-invited");
-    public static final MessageKey GUILD_FULL = MessageKey.of("guilds.invite.guild-full");
-    public static final MessageKey NO_QUEUED_ACTION = MessageKey.of("guilds.confirm.no-action");
-}
+//package me.glaremasters.guilds.util;
+//
+//import co.aikar.locales.MessageKey;
+//
+//public class GuildMessageKeys {
+//
+//    public static final MessageKey INVALID_GUILD_NAME = MessageKey.of("guilds.invalid-guild-name");
+//    public static final MessageKey ALREADY_A_MEMBER = MessageKey.of("guilds.invite.already-a-member");
+//    public static final MessageKey NOT_INVITED = MessageKey.of("guilds.invite.not-invited");
+//    public static final MessageKey GUILD_FULL = MessageKey.of("guilds.invite.guild-full");
+//    public static final MessageKey NO_QUEUED_ACTION = MessageKey.of("guilds.confirm.no-action");
+//}
