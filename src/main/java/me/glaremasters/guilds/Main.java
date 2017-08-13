2cce9b3358afd93a20f92ca3e63bbe96d8f8f7fd
darbyjack <admin@glaremasters.me>
2017-08-13 14:52:31 -0500

    Updated config version

--- src/main/java/me/glaremasters/guilds/Main.java
+++ src/main/java/me/glaremasters/guilds/Main.java
@@ -124,7 +124,7 @@ public class Main extends JavaPlugin {
     taskChainFactory = BukkitTaskChainFactory.create(this);
 
     setDatabaseType();
-    if (!getConfig().isSet("version") || getConfig().getInt("version") != 11) {
+    if (!getConfig().isSet("version") || getConfig().getInt("version") != 12) {
       Bukkit.getConsoleSender().sendMessage(
           "§a[Guilds] §3The config is not on the latest version! Please back up and delete the config.yml and allow the new one to generate, otherwise the plugin may not work as intended!");
     }
