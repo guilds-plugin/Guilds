ea93d1bd9f184d87b680a39c6369319c2a96fe86
Senroht <sebastianhoye@gmail.com>
2017-08-20 19:35:40 +0100

    fixes #40, only been tested with 3 players

--- src/main/java/me/glaremasters/guilds/commands/CommandInvite.java
+++ src/main/java/me/glaremasters/guilds/commands/CommandInvite.java
@@ -45,7 +45,10 @@ public class CommandInvite extends CommandBase {
             Message.sendMessage(player, Message.COMMAND_INVITE_ALREADY_IN_GUILD);
             return;
         }
-
+        if (guild.getInvitedMembers().contains(invitedPlayer.getUniqueId())){
+            Message.sendMessage(player, Message.COMMAND_INVITE_ALREADY_INVITED);
+            return;
+        }
         GuildInviteEvent event = new GuildInviteEvent(player, guild, invitedPlayer);
         Main.getInstance().getServer().getPluginManager().callEvent(event);
         if (event.isCancelled()) {
