4c8f5418135066eb5ecae0486b9c083af63b4670
darbyjack <admin@glaremasters.me>
2017-10-29 15:47:10 -0500

    Started on 1.9.2-BETA

--- src/main/java/me/glaremasters/guilds/Main.java
+++ src/main/java/me/glaremasters/guilds/Main.java
@@ -89,7 +89,7 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         if (getConfig().getBoolean("announcements.console")) {
             try {
-                URL url = new URL("https://glaremasters.me/guilds/announcements/1.9.0");
+                URL url = new URL("https://glaremasters.me/guilds/announcements/1.9.2");
                 URLConnection con = url.openConnection();
                 con.setRequestProperty("User-Agent",
                         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
