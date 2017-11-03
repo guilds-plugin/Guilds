7d22ec4a95618e77d7884fdeda34bb4f0b2391ed
darbyjack <admin@glaremasters.me>
2017-11-02 20:47:03 -0500

    Only get told announcements once each reboot (per person)

--- src/main/java/me/glaremasters/guilds/listeners/AnnouncementListener.java
+++ src/main/java/me/glaremasters/guilds/listeners/AnnouncementListener.java
@@ -3,6 +3,9 @@ package me.glaremasters.guilds.listeners;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 import me.glaremasters.guilds.Main;
 import org.apache.commons.io.IOUtils;
 import org.bukkit.entity.Player;
@@ -15,6 +18,8 @@ import org.bukkit.event.player.PlayerJoinEvent;
  */
 public class AnnouncementListener implements Listener {
 
+    public static Set<UUID> ALREADY_INFORMED = new HashSet<>();
+
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         String URL = "https://glaremasters.me/guilds/announcements/" + Main.getInstance().getDescription().getVersion();
@@ -23,18 +28,21 @@ public class AnnouncementListener implements Listener {
                 .scheduleSyncDelayedTask(Main.getInstance(), () -> {
                     if (Main.getInstance().getConfig().getBoolean("announcements.in-game")) {
                         if (player.isOp()) {
-                            try {
-                                URL url = new URL(URL);
-                                URLConnection con = url.openConnection();
-                                con.setRequestProperty("User-Agent",
-                                        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-                                InputStream in = con.getInputStream();
-                                String encoding = con.getContentEncoding();
-                                encoding = encoding == null ? "UTF-8" : encoding;
-                                String body = IOUtils.toString(in, encoding);
-                                player.sendMessage(body);
-                            } catch (Exception exception) {
-                                exception.printStackTrace();
+                            if (!ALREADY_INFORMED.contains(player.getUniqueId())) {
+                                try {
+                                    URL url = new URL(URL);
+                                    URLConnection con = url.openConnection();
+                                    con.setRequestProperty("User-Agent",
+                                            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+                                    InputStream in = con.getInputStream();
+                                    String encoding = con.getContentEncoding();
+                                    encoding = encoding == null ? "UTF-8" : encoding;
+                                    String body = IOUtils.toString(in, encoding);
+                                    player.sendMessage(body);
+                                    ALREADY_INFORMED.add(player.getUniqueId());
+                                } catch (Exception exception) {
+                                    exception.printStackTrace();
+                                }
                             }
                         }
                     }
