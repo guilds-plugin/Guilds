c666623b41ac365e0cf218cd123993465a838c88
darbyjack <admin@glaremasters.me>
2017-11-19 16:40:55 -0600

    Finished rewriting guild claims

--- src/main/java/me/glaremasters/guilds/commands/CommandClaim.java
+++ src/main/java/me/glaremasters/guilds/commands/CommandClaim.java
@@ -66,14 +66,20 @@ public class CommandClaim extends CommandBase {
             Message.sendMessage(player, Message.COMMAND_CLAIM_WORLDGUARD_REQUIRED);
             return;
         }
+        double claimCost;
 
         if (config.getBoolean("custom-claim-size")) {
-            if (args[0] == null) {
+            if (args.length != 1) {
                 Message.sendMessage(player, Message.COMMAND_CLAIM_SIZE_REQUIRED);
                 return;
             }
-            double claimCost = (config.getDouble("custom-claim-size") * Double.valueOf(args[0]));
+            if (Integer.valueOf(args[0]) > config.getInt("custom-max-claim-size")) {
+                Message.sendMessage(player, Message.COMMAND_CLAIM_TOO_BIG
+                        .replace("{max}", String.valueOf(config.getInt("custom-max-claim-size"))));
+                return;
+            }
 
+            claimCost = (config.getDouble("custom-claim-price") * Integer.valueOf(args[0]));
             if (Main.vault && claimCost != -1) {
                 if (Main.getInstance().getEconomy().getBalance(player) < claimCost) {
                     Message.sendMessage(player, Message.COMMAND_ERROR_NOT_ENOUGH_MONEY);
@@ -82,93 +88,183 @@ public class CommandClaim extends CommandBase {
 
                 Message.sendMessage(player, Message.COMMAND_CREATE_MONEY_WARNING_SETHOME
                         .replace("{amount}", String.valueOf(claimCost)));
-            } else {
-                Message.sendMessage(player, Message.COMMAND_CREATE_WARNING);
+
+
+            }
+        } else {
+            claimCost = config.getDouble("regular-claim-price");
+            if (Main.vault && claimCost != -1) {
+                if (Main.getInstance().getEconomy().getBalance(player) < claimCost) {
+                    Message.sendMessage(player, Message.COMMAND_ERROR_NOT_ENOUGH_MONEY);
+                    return;
+                }
+                Message.sendMessage(player, Message.COMMAND_CREATE_MONEY_WARNING_SETHOME
+                        .replace("{amount}", String.valueOf(claimCost)));
+
             }
         }
-
-
-        double claimCost = (config.getDouble("custom-claim-size") * Double.valueOf(args[0]));
 
         Main.getInstance().getCommandHandler().addAction(player, new ConfirmAction() {
             @Override
             public void accept() {
-
-                EconomyResponse response =
-                        Main.getInstance().getEconomy().withdrawPlayer(player, claimCost);
-                if (!response.transactionSuccess()) {
-                    Message.sendMessage(player, Message.COMMAND_ERROR_NOT_ENOUGH_MONEY);
-                    return;
+                if (config.getBoolean("custom-claim-size")) {
+                    double claimCost = (config.getDouble("custom-claim-price") * Double
+                            .valueOf(args[0]));
+                    EconomyResponse response =
+                            Main.getInstance().getEconomy().withdrawPlayer(player, claimCost);
+                    if (!response.transactionSuccess()) {
+                        Message.sendMessage(player, Message.COMMAND_ERROR_NOT_ENOUGH_MONEY);
+                        return;
+                    }
+                } else {
+                    double claimCost2 = config.getDouble("regular-claim-price");
+                    EconomyResponse response2 =
+                            Main.getInstance().getEconomy().withdrawPlayer(player, claimCost2);
+                    if (!response2.transactionSuccess()) {
+                        Message.sendMessage(player, Message.COMMAND_ERROR_NOT_ENOUGH_MONEY);
+                        return;
+                    }
                 }
+                if (!config.getBoolean("custom-claim-size")) {
+                    BlockVector min = new BlockVector((player.getLocation().getX() - (
+                            config.getInt("regular-claim-size") / 2)), 0,
+                            (player.getLocation().getZ() - (config.getInt("regular-claim-size")
+                                    / 2)));
+                    BlockVector max = new BlockVector((player.getLocation().getX() + (
+                            config.getInt("regular-claim-size") / 2)), 255,
+                            (player.getLocation().getZ() + (config.getInt("regular-claim-size")
+                                    / 2)));
+                    ProtectedRegion region = new ProtectedCuboidRegion(guild.getName(), min, max);
+                    RegionContainer container = getWorldGuard().getRegionContainer();
+                    RegionManager regions = container.get(player.getWorld());
 
-                BlockVector min = new BlockVector((player.getLocation().getX() - (
-                        config.getInt("claims.size") / 2)), 0,
-                        (player.getLocation().getZ() - (config.getInt("claims.size") / 2)));
-                BlockVector max = new BlockVector((player.getLocation().getX() + (
-                        config.getInt("claims.size") / 2)), 255,
-                        (player.getLocation().getZ() + (config.getInt("claims.size") / 2)));
-                ProtectedRegion region = new ProtectedCuboidRegion(guild.getName(), min, max);
-                RegionContainer container = getWorldGuard().getRegionContainer();
-                RegionManager regions = container.get(player.getWorld());
+                    if (region != null) {
+                        regions.removeRegion(guild.getName());
+                    }
 
-                if (region != null) {
-                    regions.removeRegion(guild.getName());
+                    ApplicableRegionSet set = regions.getApplicableRegions(region);
+                    if (set.size() > 0) {
+                        Message.sendMessage(player, Message.COMMAND_CLAIM_TOO_CLOSE);
+                        return;
+                    }
+
+                    regions.addRegion(region);
+                    Message.sendMessage(player, Message.COMMAND_CLAIM_COORDINATES);
+                    player.sendMessage(ChatColor.BLUE + "" + Math
+                            .ceil((player.getLocation().getX() - (
+                                    config.getInt("regular-claim-size")
+                                            / 2))) + ", " + "0.0" + ", " +
+                            Math.ceil((player.getLocation().getZ() - (
+                                    config.getInt("regular-claim-size")
+                                            / 2))) + ChatColor.GREEN + " to " +
+                            ChatColor.BLUE + (Math.ceil((player.getLocation().getX() + (
+                            config.getInt("regular-claim-size") / 2))) + ", " + "255.0, "
+                            + (Math
+                            .ceil((player.getLocation().getZ() + (
+                                    config.getInt("regular-claim-size")
+                                            / 2))))));
+                    region.setFlag(DefaultFlag.GREET_MESSAGE,
+                            "Entering " + guild.getName() + "'s base");
+                    region.setFlag(DefaultFlag.FAREWELL_MESSAGE,
+                            "Leaving " + guild.getName() + "'s base");
+
+                    ProtectedRegion regionTest = regions.getRegion(guild.getName());
+                    Location outlineMin = new Location(player.getWorld(), 0, 0, 0);
+                    outlineMin.setX(regionTest.getMinimumPoint().getX());
+                    outlineMin.setY(player.getLocation().getY());
+                    outlineMin.setZ(regionTest.getMinimumPoint().getZ());
+
+                    Location outlineMax = new Location(player.getWorld(), 0, 0, 0);
+                    outlineMax.setX(regionTest.getMaximumPoint().getX());
+                    outlineMax.setY(player.getLocation().getY());
+                    outlineMax.setZ(regionTest.getMaximumPoint().getZ());
+
+                    for (double x1 = 0; x1 <= outlineMax.getX() - outlineMin.getX(); x1++) {
+                        player.sendBlockChange(outlineMin.clone().add(x1, 0, 0), Material.DIRT,
+                                (byte) 0);
+                    }
+
+                    for (double z = 0; z <= outlineMax.getZ() - outlineMin.getZ(); z++) {
+                        player.sendBlockChange(outlineMin.clone().add(0, 0, z), Material.DIRT,
+                                (byte) 0);
+                    }
+
+                    DefaultDomain members = region.getMembers();
+                    DefaultDomain owners = region.getOwners();
+                    owners.addPlayer(player.getName());
+                    guild.getMembers().stream()
+                            .map(member -> Bukkit.getOfflinePlayer(member.getUniqueId()))
+                            .forEach(member -> {
+                                members.addPlayer(member.getName());
+                            });
+                } else {
+                    BlockVector min = new BlockVector(
+                            (player.getLocation().getX() - (Integer.valueOf(args[0]) / 2)), 0,
+                            (player.getLocation().getZ() - (Integer.valueOf(args[0]) / 2)));
+                    BlockVector max = new BlockVector((player.getLocation().getX() + (
+                            Integer.valueOf(args[0]) / 2)), 255,
+                            (player.getLocation().getZ() + (Integer.valueOf(args[0]) / 2)));
+                    ProtectedRegion region = new ProtectedCuboidRegion(guild.getName(), min, max);
+                    RegionContainer container = getWorldGuard().getRegionContainer();
+                    RegionManager regions = container.get(player.getWorld());
+
+                    if (region != null) {
+                        regions.removeRegion(guild.getName());
+                    }
+
+                    ApplicableRegionSet set = regions.getApplicableRegions(region);
+                    if (set.size() > 0) {
+                        Message.sendMessage(player, Message.COMMAND_CLAIM_TOO_CLOSE);
+                        return;
+                    }
+
+                    regions.addRegion(region);
+                    Message.sendMessage(player, Message.COMMAND_CLAIM_COORDINATES);
+                    player.sendMessage(ChatColor.BLUE + "" + Math
+                            .ceil((player.getLocation().getX() - (Integer.valueOf(args[0])
+                                    / 2))) + ", " + "0.0" + ", " +
+                            Math.ceil((player.getLocation().getZ() - (Integer.valueOf(args[0])
+                                    / 2))) + ChatColor.GREEN + " to " +
+                            ChatColor.BLUE + (Math.ceil((player.getLocation().getX() + (
+                            Integer.valueOf(args[0]) / 2))) + ", " + "255.0, "
+                            + (Math
+                            .ceil((player.getLocation().getZ() + (Integer.valueOf(args[0])
+                                    / 2))))));
+                    region.setFlag(DefaultFlag.GREET_MESSAGE,
+                            "Entering " + guild.getName() + "'s base");
+                    region.setFlag(DefaultFlag.FAREWELL_MESSAGE,
+                            "Leaving " + guild.getName() + "'s base");
+
+                    ProtectedRegion regionTest = regions.getRegion(guild.getName());
+                    Location outlineMin = new Location(player.getWorld(), 0, 0, 0);
+                    outlineMin.setX(regionTest.getMinimumPoint().getX());
+                    outlineMin.setY(player.getLocation().getY());
+                    outlineMin.setZ(regionTest.getMinimumPoint().getZ());
+
+                    Location outlineMax = new Location(player.getWorld(), 0, 0, 0);
+                    outlineMax.setX(regionTest.getMaximumPoint().getX());
+                    outlineMax.setY(player.getLocation().getY());
+                    outlineMax.setZ(regionTest.getMaximumPoint().getZ());
+
+                    for (double x1 = 0; x1 <= outlineMax.getX() - outlineMin.getX(); x1++) {
+                        player.sendBlockChange(outlineMin.clone().add(x1, 0, 0), Material.DIRT,
+                                (byte) 0);
+                    }
+
+                    for (double z = 0; z <= outlineMax.getZ() - outlineMin.getZ(); z++) {
+                        player.sendBlockChange(outlineMin.clone().add(0, 0, z), Material.DIRT,
+                                (byte) 0);
+                    }
+
+                    DefaultDomain members = region.getMembers();
+                    DefaultDomain owners = region.getOwners();
+                    owners.addPlayer(player.getName());
+                    guild.getMembers().stream()
+                            .map(member -> Bukkit.getOfflinePlayer(member.getUniqueId()))
+                            .forEach(member -> {
+                                members.addPlayer(member.getName());
+                            });
                 }
-
-                ApplicableRegionSet set = regions.getApplicableRegions(region);
-                if (set.size() > 0) {
-                    Message.sendMessage(player, Message.COMMAND_CLAIM_TOO_CLOSE);
-                    return;
-                }
-
-                regions.addRegion(region);
-                Message.sendMessage(player, Message.COMMAND_CLAIM_COORDINATES);
-                player.sendMessage(ChatColor.BLUE + "" + Math
-                        .ceil((player.getLocation().getX() - (config.getInt("claims.size")
-                                / 2))) + ", " + "0.0" + ", " +
-                        Math.ceil((player.getLocation().getZ() - (config.getInt("claims.size")
-                                / 2))) + ChatColor.GREEN + " to " +
-                        ChatColor.BLUE + (Math.ceil((player.getLocation().getX() + (
-                        config.getInt("claims.size") / 2))) + ", " + "255.0, "
-                        + (Math
-                        .ceil((player.getLocation().getZ() + (config.getInt("claims.size")
-                                / 2))))));
-                region.setFlag(DefaultFlag.GREET_MESSAGE,
-                        "Entering " + guild.getName() + "'s base");
-                region.setFlag(DefaultFlag.FAREWELL_MESSAGE,
-                        "Leaving " + guild.getName() + "'s base");
-
-                ProtectedRegion regionTest = regions.getRegion(guild.getName());
-                Location outlineMin = new Location(player.getWorld(), 0, 0, 0);
-                outlineMin.setX(regionTest.getMinimumPoint().getX());
-                outlineMin.setY(player.getLocation().getY());
-                outlineMin.setZ(regionTest.getMinimumPoint().getZ());
-
-                Location outlineMax = new Location(player.getWorld(), 0, 0, 0);
-                outlineMax.setX(regionTest.getMaximumPoint().getX());
-                outlineMax.setY(player.getLocation().getY());
-                outlineMax.setZ(regionTest.getMaximumPoint().getZ());
-
-                for (double x1 = 0; x1 <= outlineMax.getX() - outlineMin.getX(); x1++) {
-                    player.sendBlockChange(outlineMin.clone().add(x1, 0, 0), Material.DIRT,
-                            (byte) 0);
-                }
-
-                for (double z = 0; z <= outlineMax.getZ() - outlineMin.getZ(); z++) {
-                    player.sendBlockChange(outlineMin.clone().add(0, 0, z), Material.DIRT,
-                            (byte) 0);
-                }
-
-                DefaultDomain members = region.getMembers();
-                DefaultDomain owners = region.getOwners();
-                owners.addPlayer(player.getName());
-                guild.getMembers().stream()
-                        .map(member -> Bukkit.getOfflinePlayer(member.getUniqueId()))
-                        .forEach(member -> {
-                            members.addPlayer(member.getName());
-                        });
-
-
             }
 
             @Override
