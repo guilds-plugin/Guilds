e675f23366bcd4c305790e6a284fb0363f770170
darbyjack <admin@glaremasters.me>
2018-01-05 23:48:53 -0600

    Allow users to customize the material for each buff

--- src/main/java/me/glaremasters/guilds/commands/CommandBuff.java
+++ src/main/java/me/glaremasters/guilds/commands/CommandBuff.java
@@ -9,6 +9,7 @@ import me.glaremasters.guilds.message.Message;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -54,135 +55,118 @@ public class CommandBuff extends CommandBase {
 
         // Buff 1: Haste
         ArrayList<String> haste = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.haste").stream()
+        FileConfiguration config = Guilds.getInstance().getConfig();
+        config.getStringList("buff.description.haste").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it)).forEach(haste::add);
         haste.add("");
         haste.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.haste")));
+                config.getString("buff.description.price") + config.getInt("buff.price.haste")));
         haste.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.haste")));
-        buff.setItem(0, createItemStack(Material.FEATHER,
-                Guilds.getInstance().getConfig().getString("buff.name.haste"), haste));
+                config.getString("buff.description.length") + config.getInt("buff.time.haste")));
+        buff.setItem(0, createItemStack(Material.getMaterial(config.getString("buff.icon.haste")),
+                config.getString("buff.name.haste"), haste));
 
         // Buff 2: Walk Speed
         ArrayList<String> speed = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.speed").stream()
+        config.getStringList("buff.description.speed").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it)).forEach(speed::add);
         speed.add("");
         speed.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.speed")));
+                config.getString("buff.description.price") + config.getInt("buff.price.speed")));
         speed.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.speed")));
-        buff.setItem(4, createItemStack(Material.SUGAR,
-                Guilds.getInstance().getConfig().getString("buff.name.speed"), speed));
+                config.getString("buff.description.length") + config.getInt("buff.time.speed")));
+        buff.setItem(4, createItemStack(Material.getMaterial(config.getString("buff.icon.speed")),
+                config.getString("buff.name.speed"), speed));
 
         // Buff 3: Fire Resistance
         ArrayList<String> fireResistance = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.fire-resistance").stream()
+        config.getStringList("buff.description.fire-resistance").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it))
                 .forEach(fireResistance::add);
         fireResistance.add("");
         fireResistance.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.fire-resistance")));
+                config.getString("buff.description.price") + config.getInt("buff.price.fire-resistance")));
         fireResistance.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.fire-resistance")));
-        buff.setItem(2, createItemStack(Material.BLAZE_POWDER,
-                Guilds.getInstance().getConfig().getString("buff.name.fire-resistance"),
+                config.getString("buff.description.length") + config.getInt("buff.time.fire-resistance")));
+        buff.setItem(2, createItemStack(Material.getMaterial(config.getString("buff.icon.fire-resistance")),
+                config.getString("buff.name.fire-resistance"),
                 fireResistance));
 
         // Buff 4: Night Vision
         ArrayList<String> nightvision = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.night-vision").stream()
+        config.getStringList("buff.description.night-vision").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it))
                 .forEach(nightvision::add);
         nightvision.add("");
         nightvision.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.night-vision")));
+                config.getString("buff.description.price") + config.getInt("buff.price.night-vision")));
         nightvision.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.night-vision")));
-        buff.setItem(6, createItemStack(Material.REDSTONE_TORCH_ON,
-                Guilds.getInstance().getConfig().getString("buff.name.night-vision"), nightvision));
+                config.getString("buff.description.length") + config.getInt("buff.time.night-vision")));
+        buff.setItem(6, createItemStack(Material.getMaterial(config.getString("buff.icon.night-vision")),
+                config.getString("buff.name.night-vision"), nightvision));
 
         // Buff 5: Invisibility
         ArrayList<String> invisibility = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.invisibility").stream()
+        config.getStringList("buff.description.invisibility").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it))
                 .forEach(invisibility::add);
         invisibility.add("");
         invisibility.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.invisibility")));
+                config.getString("buff.description.price") + config.getInt("buff.price.invisibility")));
         invisibility.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.invisibility")));
-        buff.setItem(8, createItemStack(Material.EYE_OF_ENDER,
-                Guilds.getInstance().getConfig().getString("buff.name.invisibility"), invisibility));
+                config.getString("buff.description.length") + config.getInt("buff.time.invisibility")));
+        buff.setItem(8, createItemStack(Material.getMaterial(config.getString("buff.icon.invisibility")),
+                config.getString("buff.name.invisibility"), invisibility));
 
         // Buff 6: Strength
         ArrayList<String> strength = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.strength").stream()
+        config.getStringList("buff.description.strength").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it)).forEach(strength::add);
         strength.add("");
         strength.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.strength")));
+                config.getString("buff.description.price") + config.getInt("buff.price.strength")));
         strength.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.strength")));
-        buff.setItem(1, createItemStack(Material.DIAMOND_SWORD,
-                Guilds.getInstance().getConfig().getString("buff.name.strength"), strength));
+                config.getString("buff.description.length") + config.getInt("buff.time.strength")));
+        buff.setItem(1, createItemStack(Material.getMaterial(config.getString("buff.icon.strength")),
+                config.getString("buff.name.strength"), strength));
 
         // Buff 7: Jump
         ArrayList<String> jump = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.jump").stream()
+        config.getStringList("buff.description.jump").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it)).forEach(jump::add);
         jump.add("");
         jump.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.jump")));
+                config.getString("buff.description.price") + config.getInt("buff.price.jump")));
         jump.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.jump")));
-        buff.setItem(3, createItemStack(Material.DIAMOND_BOOTS,
-                Guilds.getInstance().getConfig().getString("buff.name.jump"), jump));
+                config.getString("buff.description.length") + config.getInt("buff.time.jump")));
+        buff.setItem(3, createItemStack(Material.getMaterial(config.getString("buff.icon.jump")),
+                config.getString("buff.name.jump"), jump));
 
         // Buff 8: Water Breathing
         ArrayList<String> waterbreathing = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.water-breathing").stream()
+        config.getStringList("buff.description.water-breathing").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it))
                 .forEach(waterbreathing::add);
         waterbreathing.add("");
         waterbreathing.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.water-breathing")));
+                config.getString("buff.description.price") + config.getInt("buff.price.water-breathing")));
         waterbreathing.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.water-breathing")));
-        buff.setItem(5, createItemStack(Material.BUCKET,
-                Guilds.getInstance().getConfig().getString("buff.name.water-breathing"),
+                config.getString("buff.description.length") + config.getInt("buff.time.water-breathing")));
+        buff.setItem(5, createItemStack(Material.getMaterial(config.getString("buff.icon.water-breathing")),
+                config.getString("buff.name.water-breathing"),
                 waterbreathing));
 
         // Buff 9: Luck
         ArrayList<String> regeneration = new ArrayList<String>();
-        Guilds.getInstance().getConfig().getStringList("buff.description.regeneration").stream()
+        config.getStringList("buff.description.regeneration").stream()
                 .map(it -> ChatColor.translateAlternateColorCodes('&', it)).forEach(regeneration::add);
         regeneration.add("");
         regeneration.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.price") + Guilds
-                        .getInstance().getConfig().getInt("buff.price.regeneration")));
+                config.getString("buff.description.price") + config.getInt("buff.price.regeneration")));
         regeneration.add(ChatColor.translateAlternateColorCodes('&',
-                Guilds.getInstance().getConfig().getString("buff.description.length") + Guilds
-                        .getInstance().getConfig().getInt("buff.time.regeneration")));
-        buff.setItem(7, createItemStack(Material.EMERALD,
-                Guilds.getInstance().getConfig().getString("buff.name.regeneration"), regeneration));
+                config.getString("buff.description.length") + config.getInt("buff.time.regeneration")));
+        buff.setItem(7, createItemStack(Material.getMaterial(config.getString("buff.icon.regeneration")),
+                config.getString("buff.name.regeneration"), regeneration));
 
         // Open inventory
         player.openInventory(buff);
