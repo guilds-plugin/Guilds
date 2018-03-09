8b601ba9a7f8d2e41e30c0d8a6b0a7a134f55091
darbyjack <admin@glaremasters.me>
2018-03-09 12:11:40 -0600

    Redesigned ChatListener

--- src/main/java/me/glaremasters/guilds/listeners/ChatListener.java
+++ src/main/java/me/glaremasters/guilds/listeners/ChatListener.java
@@ -4,6 +4,7 @@ import static me.glaremasters.guilds.placeholders.PlaceholdersSRV.getGuildMember
 import static me.glaremasters.guilds.placeholders.PlaceholdersSRV.getGuildMembersOnline;
 import static me.glaremasters.guilds.placeholders.PlaceholdersSRV.getGuildRole;
 import me.glaremasters.guilds.guild.Guild;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,30 +16,29 @@ public class ChatListener implements Listener {
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
+
         Player player = event.getPlayer();
         Guild guild = Guild.getGuild(player.getUniqueId());
-        if (guild == null) {
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD}", ""));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_PREFIX}", ""));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MASTER}", ""));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBER_COUNT}", ""));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBERS_ONLINE}", ""));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_STATUS}", ""));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_ROLE}", ""));
-        } else {
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD}", guild.getName()));
-            event.setFormat(
-                    event.getFormat().replace("{ESSENTIALS_GUILD_PREFIX}", guild.getPrefix()));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBER_COUNT}",
-                    getGuildMemberCount(event.getPlayer())));
-            event.setFormat(event.getFormat().replace("{ESSENTIALS_GUILD_MEMBERS_ONLINE}",
-                    getGuildMembersOnline(event.getPlayer())));
-            event.setFormat(
-                    event.getFormat().replace("{ESSENTIALS_GUILD_STATUS}", guild.getStatus()));
-            event.setFormat(
-                    event.getFormat()
-                            .replace("{ESSENTIALS_GUILD_ROLE}", getGuildRole(event.getPlayer())));
 
+        String message = event.getFormat();
+
+        if (guild == null) {
+            String regex = "(\\{ESSENTIALS_GUILD(?:.+)?})";
+
+            event.setFormat(message.replaceAll(regex, ""));
+            return;
         }
+
+        message = message
+                .replace("{ESSENTIALS_GUILD}", guild.getName())
+                .replace("{ESSENTIALS_GUILD_PREFIX}", guild.getPrefix())
+                .replace("{ESSENTIALS_GUILD_MASTER}", Bukkit.getOfflinePlayer(guild.getGuildMaster().getUniqueId()).getName())
+                .replace("{ESSENTIALS_GUILD_MEMBER_COUNT}", getGuildMemberCount(event.getPlayer()))
+                .replace("{ESSENTIALS_GUILD_MEMBERS_ONLINE}", getGuildMembersOnline(event.getPlayer()))
+                .replace("{ESSENTIALS_GUILD_STATUS}", guild.getStatus())
+                .replace("{ESSENTIALS_GUILD_ROLE}", getGuildRole(event.getPlayer()));
+
+        event.setFormat(message);
     }
+
 }
