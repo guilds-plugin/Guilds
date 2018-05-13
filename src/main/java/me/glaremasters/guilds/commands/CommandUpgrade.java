9908ee749e8bf90dd508614b0f9405a0674a1de7
darbyjack <admin@glaremasters.me>
2018-05-13 16:53:30 -0500

    Removed using personal balance for guild upgrade

--- src/main/java/me/glaremasters/guilds/commands/CommandUpgrade.java
+++ src/main/java/me/glaremasters/guilds/commands/CommandUpgrade.java
@@ -7,7 +7,6 @@ import me.glaremasters.guilds.guild.GuildRole;
 import me.glaremasters.guilds.handlers.TitleHandler;
 import me.glaremasters.guilds.message.Message;
 import me.glaremasters.guilds.util.ConfirmAction;
-import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -27,7 +26,7 @@ public class CommandUpgrade extends CommandBase {
 
     @Override
     public void execute(Player player, String[] args) {
-        final FileConfiguration config = Guilds.getInstance().getConfig();
+        final FileConfiguration c = Guilds.getInstance().getConfig();
         Guild guild = Guild.getGuild(player.getUniqueId());
 
         if (guild == null) {
@@ -42,111 +41,69 @@ public class CommandUpgrade extends CommandBase {
             return;
         }
 
-        if (guild.getTier() >= Guilds.getInstance().getConfig().getInt("max-number-of-tiers")) {
+        if (guild.getTier() >= c.getInt("max-number-of-tiers")) {
             Message.sendMessage(player, Message.COMMAND_UPGRADE_TIER_MAX);
             return;
         }
 
-        if (guild.getMemebersRankupRequire() != 0 && guild.getMembers().size() < guild.getMemebersRankupRequire()) {
-            Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MEMBERS.replace("{amount}", String.valueOf(guild.getMemebersRankupRequire())));
+        if (guild.getMemebersRankupRequire() != 0 && guild.getMembers().size() < guild
+                .getMemebersRankupRequire()) {
+            Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MEMBERS
+                    .replace("{amount}", String.valueOf(guild.getMemebersRankupRequire())));
             return;
         }
         double balance = guild.getBankBalance();
         double tierUpgradeCost = guild.getTierCost();
-        if (Guilds.getInstance().getConfig().getBoolean("use-bank-balance")) {
-            if (balance < tierUpgradeCost) {
-                double needed = (tierUpgradeCost - balance);
-                Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MONEY
-                        .replace("{needed}", Double.toString(needed)));
-                return;
-            }
-            Message.sendMessage(player, Message.COMMAND_UPGRADE_MONEY_WARNING
-                    .replace("{amount}", String.valueOf(tierUpgradeCost)));
-            Guilds.getInstance().getCommandHandler().addAction(player, new ConfirmAction() {
-                @Override
-                public void accept() {
-                    if (balance < tierUpgradeCost) {
-                        double needed = (tierUpgradeCost - balance);
-                        Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MONEY
-                                .replace("{needed}", Double.toString(needed)));
-                        return;
-                    }
-                    Message.sendMessage(player, Message.COMMAND_UPGRADE_SUCCESS);
-                    Guilds.getInstance().guildTiersConfig.set(guild.getName(), tier + 1);
-                    Guilds.getInstance().guildBanksConfig
-                            .set(guild.getName(), balance - tierUpgradeCost);
-                    Guilds.getInstance().saveGuildData();
-                    TitleHandler.tierTitles(player);
-                    guild.updateGuild("");
-                }
-
-
-                @Override
-                public void decline() {
-                    Message.sendMessage(player, Message.COMMAND_UPGRADE_CANCEL);
-                    Guilds.getInstance().getCommandHandler().removeAction(player);
-                }
-            });
-        } else {
-            if (Guilds.vaultEconomy && tierUpgradeCost != -1) {
-                if (Guilds.getInstance().getEconomy().getBalance(player) < tierUpgradeCost) {
-                    double needed = (tierUpgradeCost - Guilds.getInstance().getEconomy()
-                            .getBalance(player));
+        if (balance < tierUpgradeCost) {
+            double needed = (tierUpgradeCost - balance);
+            Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MONEY
+                    .replace("{needed}", Double.toString(needed)));
+            return;
+        }
+        Message.sendMessage(player, Message.COMMAND_UPGRADE_MONEY_WARNING
+                .replace("{amount}", String.valueOf(tierUpgradeCost)));
+        Guilds.getInstance().getCommandHandler().addAction(player, new ConfirmAction() {
+            @Override
+            public void accept() {
+                if (balance < tierUpgradeCost) {
+                    double needed = (tierUpgradeCost - balance);
                     Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MONEY
                             .replace("{needed}", Double.toString(needed)));
                     return;
                 }
-
-                Message.sendMessage(player, Message.COMMAND_UPGRADE_MONEY_WARNING
-                        .replace("{amount}", String.valueOf(tierUpgradeCost)));
-            } else {
-                Message.sendMessage(player, Message.COMMAND_CREATE_WARNING);
-            }
-
-            Guilds.getInstance().getCommandHandler().addAction(player, new ConfirmAction() {
-                @Override
-                public void accept() {
-                    if (Guilds.getInstance().getEconomy().getBalance(player) < tierUpgradeCost) {
-                        Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MONEY);
-                        return;
-                    }
-                    Message.sendMessage(player, Message.COMMAND_UPGRADE_SUCCESS);
-                    if (!Guilds.getInstance().getConfig().getBoolean("carry-over-perms")) {
-                        guild.getMembers().stream().map(member -> Bukkit.getOfflinePlayer(member.getUniqueId()))
-                                .forEach(member -> {
-                                    for (String perms : guild.getGuildPerms()) {
-                                        Guilds.getPermissions().playerRemove(null, member, perms);
-                                    }
-                                });
-                    }
-                    Guilds.getInstance().guildTiersConfig.set(guild.getName(), tier + 1);
-                    Guilds.getInstance().saveGuildData();
-                    guild.getMembers().stream().map(member -> Bukkit.getOfflinePlayer(member.getUniqueId()))
+                Message.sendMessage(player, Message.COMMAND_UPGRADE_SUCCESS);
+                if (!c.getBoolean("carry-over-perms")) {
+                    guild.getMembers().stream()
+                            .map(member -> Bukkit.getOfflinePlayer(member.getUniqueId()))
                             .forEach(member -> {
                                 for (String perms : guild.getGuildPerms()) {
-                                    Guilds.getPermissions().playerAdd(null, member, perms);
+                                    Guilds.getPermissions().playerRemove(null, member, perms);
                                 }
                             });
-                    EconomyResponse response =
-                            Guilds.getInstance().getEconomy().withdrawPlayer(player, tierUpgradeCost);
-                    if (!response.transactionSuccess()) {
-                        Message.sendMessage(player, Message.COMMAND_UPGRADE_NOT_ENOUGH_MONEY);
-                        return;
-                    }
-
-                    TitleHandler.tierTitles(player);
-                    guild.updateGuild("");
                 }
+                Guilds.getInstance().guildTiersConfig.set(guild.getName(), tier + 1);
+                Guilds.getInstance().guildBanksConfig
+                        .set(guild.getName(), balance - tierUpgradeCost);
+                Guilds.getInstance().saveGuildData();
+                guild.getMembers().stream()
+                        .map(member -> Bukkit.getOfflinePlayer(member.getUniqueId()))
+                        .forEach(member -> {
+                            for (String perms : guild.getGuildPerms()) {
+                                Guilds.getPermissions().playerAdd(null, member, perms);
+                            }
+                        });
+                TitleHandler.tierTitles(player);
+                guild.updateGuild("");
+            }
+
+            @Override
+            public void decline() {
+                Message.sendMessage(player, Message.COMMAND_UPGRADE_CANCEL);
+                Guilds.getInstance().getCommandHandler().removeAction(player);
+            }
+        });
 
 
-                @Override
-                public void decline() {
-                    Message.sendMessage(player, Message.COMMAND_UPGRADE_CANCEL);
-                    Guilds.getInstance().getCommandHandler().removeAction(player);
-                }
-            });
-
-
-        }
     }
 }
+
