4f87ed70092ec44df52f7148505705bb55a0b856
darbyjack <admin@glaremasters.me>
2018-05-17 10:43:36 -0500

    Changed Guild Chat to toggle only, removed need for perm

--- src/main/java/me/glaremasters/guilds/commands/CommandChat.java
+++ src/main/java/me/glaremasters/guilds/commands/CommandChat.java
@@ -30,20 +30,15 @@ public class CommandChat extends CommandBase {
             return;
         }
 
-        if (args.length == 0) {
-            if (GUILD_CHAT_PLAYERS.contains(player.getUniqueId())) {
-                GUILD_CHAT_PLAYERS.remove(player.getUniqueId());
-                Message.sendMessage(player, Message.COMMAND_CHAT_DISABLED);
-            } else {
-                GUILD_CHAT_PLAYERS.add(player.getUniqueId());
-                Message.sendMessage(player, Message.COMMAND_CHAT_ENABLED);
-            }
-        } else {
-            String message = String.join(" ", args);
-            guild.sendMessage(Message.COMMAND_CHAT_MESSAGE.replace("{role}",
-                    GuildRole.getRole(guild.getMember(player.getUniqueId()).getRole()).getName(),
-                    "{player}", player.getName(), "{message}", message));
+        if (args.length != 0) {
+            return;
         }
-
+        if (GUILD_CHAT_PLAYERS.contains(player.getUniqueId())) {
+            GUILD_CHAT_PLAYERS.remove(player.getUniqueId());
+            Message.sendMessage(player, Message.COMMAND_CHAT_DISABLED);
+        } else {
+            GUILD_CHAT_PLAYERS.add(player.getUniqueId());
+            Message.sendMessage(player, Message.COMMAND_CHAT_ENABLED);
+        }
     }
 }
--- src/main/java/me/glaremasters/guilds/listeners/GuildChatListener.java
+++ src/main/java/me/glaremasters/guilds/listeners/GuildChatListener.java
@@ -1,12 +1,13 @@
 package me.glaremasters.guilds.listeners;
 
+import static me.glaremasters.guilds.util.ColorUtil.color;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 import me.glaremasters.guilds.Guilds;
 import me.glaremasters.guilds.guild.Guild;
 import me.glaremasters.guilds.guild.GuildRole;
-import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -26,6 +27,8 @@ public class GuildChatListener implements Listener {
 
     public static final Set<UUID> GUILD_CHAT_PLAYERS = new HashSet<>();
 
+    FileConfiguration c = Guilds.getInstance().getConfig();
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
@@ -37,26 +40,11 @@ public class GuildChatListener implements Listener {
 
         if (GUILD_CHAT_PLAYERS.contains(player.getUniqueId())) {
             event.getRecipients().removeIf(r -> guild.getMember(r.getUniqueId()) == null);
-            for (Player p2 : event.getRecipients()) {
-                if (player.hasPermission("guilds.chat.color")) {
-                    p2.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                            (guilds.getConfig().getString("guild-chat-format"))
-                                    .replace("{role}", GuildRole
-                                            .getRole(
-                                                    guild.getMember(player.getUniqueId()).getRole())
-                                            .getName())
-                                    .replace("{player}", event.getPlayer().getName())
-                                    .replace("{message}", event.getMessage())));
-                } else {
-                    p2.sendMessage(guilds.getConfig().getString("guild-chat-format")
-                            .replace("{role}", GuildRole
-                                    .getRole(guild.getMember(player.getUniqueId()).getRole())
-                                    .getName()));
-                }
+            for (Player recipient : event.getRecipients()) {
+                recipient.sendMessage(color(c.getString("guild-chat-format")).replace("{role}", GuildRole.getRole(guild.getMember(player.getUniqueId()).getRole())
+                                .getName()).replace("{player}", event.getPlayer().getName()).replace("{message}", event.getMessage()));
             }
-
             event.setCancelled(true);
         }
-
     }
 }
