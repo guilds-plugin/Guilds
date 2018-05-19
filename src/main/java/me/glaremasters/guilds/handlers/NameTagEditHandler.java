944b9cb0175827c98b345c045bd0ec3240d19d56
darbyjack <admin@glaremasters.me>
2018-05-19 01:22:08 -0500

    Cleaned up NTE Handler

--- src/main/java/me/glaremasters/guilds/handlers/NameTagEditHandler.java
+++ src/main/java/me/glaremasters/guilds/handlers/NameTagEditHandler.java
@@ -1,9 +1,10 @@
 package me.glaremasters.guilds.handlers;
 
+import static me.glaremasters.guilds.util.ConfigUtil.getBoolean;
+import static me.glaremasters.guilds.util.ConfigUtil.getString;
 import com.nametagedit.plugin.NametagEdit;
 import me.glaremasters.guilds.Guilds;
 import me.glaremasters.guilds.guild.Guild;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 /**
@@ -19,20 +20,14 @@ public class NameTagEditHandler {
 
     public void setTag(Player player) {
         Guild guild = Guild.getGuild(player.getUniqueId());
-        if (plugin.getConfig().getBoolean("hooks.nametagedit")) {
-            NametagEdit.getApi()
-                    .setPrefix(player, ChatColor.translateAlternateColorCodes('&',
-                            plugin.getConfig()
-                                    .getString("nametagedit.name")
-                                    .replace("{guild}", guild.getName())
-                                    .replace("{prefix}", guild.getPrefix())));
+        if (getBoolean("hooks.nametagedit")) {
+            NametagEdit.getApi().setPrefix(player, getString("nametagedit.name").replace("{guild}", guild.getName()).replace("{prefix}", guild.getPrefix()));
         }
     }
 
     public void removeTag(Player player) {
-        if (plugin.getConfig().getBoolean("hooks.nametagedit")) {
-            NametagEdit.getApi()
-                    .setPrefix(player, "");
+        if (getBoolean("hooks.nametagedit")) {
+            NametagEdit.getApi().setPrefix(player, "");
         }
     }
 
