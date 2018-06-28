fa8404c8ac59456e35e9d5e2642327a0fba85ce9
darbyjack <admin@glaremasters.me>
2018-06-28 01:41:09 -0500

    Implemented Vault API

--- src/main/java/me/glaremasters/guilds/Guilds.java
+++ src/main/java/me/glaremasters/guilds/Guilds.java
@@ -1,21 +1,26 @@
 package me.glaremasters.guilds;
 
 import java.io.File;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Guilds extends JavaPlugin {
 
     private static Guilds guilds;
+    private static Economy econ = null;
+    private static Permission perms = null;
 
     @Override
     public void onEnable() {
-
+        guilds = this;
+        setupEconomy();
+        setupPermissions();
     }
 
     @Override
-    public void onDisable() {
-
-    }
+    public void onDisable() {}
 
     /**
      * Grabs an instance of the plugin
@@ -23,5 +28,27 @@ public final class Guilds extends JavaPlugin {
      */
     public static Guilds getGuilds() {
         return guilds;
+    }
+
+    /**
+     * Implement Vault's Economy API
+     * @return the value of the method
+     */
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) return false;
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) return false;
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    /**
+     * Implement Vault's Permission API
+     * @return the value of the method
+     */
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
     }
 }
