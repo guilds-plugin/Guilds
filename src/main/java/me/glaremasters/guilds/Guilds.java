a9597d6be362e8a96878c3d93bbc8f8822f89cd5
darbyjack <admin@glaremasters.me>
2018-06-28 01:53:46 -0500

    Implemented guilds storage file

--- src/main/java/me/glaremasters/guilds/Guilds.java
+++ src/main/java/me/glaremasters/guilds/Guilds.java
@@ -1,8 +1,10 @@
 package me.glaremasters.guilds;
 
 import java.io.File;
+import java.util.stream.Stream;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.permission.Permission;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,12 +13,16 @@ public final class Guilds extends JavaPlugin {
     private static Guilds guilds;
     private static Economy econ = null;
     private static Permission perms = null;
+    private File guild, language, languageFolder;
+    public YamlConfiguration guildConfig, languageConfig;
 
     @Override
     public void onEnable() {
         guilds = this;
         setupEconomy();
         setupPermissions();
+        initData();
+        saveData();
     }
 
     @Override
@@ -50,5 +56,25 @@ public final class Guilds extends JavaPlugin {
         RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
         perms = rsp.getProvider();
         return perms != null;
+    }
+
+    /**
+     * Initiate plugin data
+     */
+    private void initData() {
+        saveDefaultConfig();
+        this.languageFolder = new File(getDataFolder(), "languages");
+        if (!languageFolder.exists()) languageFolder.mkdirs();
+        this.guild = new File(getDataFolder(), "guilds.yml");
+        this.language = new File(languageFolder, getConfig().getString("lang") + ".yml");
+        this.languageConfig = YamlConfiguration.loadConfiguration(language);
+    }
+
+    /**
+     * Save and handle new files if needed
+     */
+    private void saveData() {
+        if (!this.guild.exists()) this.saveResource("guilds.yml", false);
+        if (!this.language.exists()) Stream.of("english").forEach(l -> this.saveResource("languages/" + l + ".yml", false));
     }
 }
