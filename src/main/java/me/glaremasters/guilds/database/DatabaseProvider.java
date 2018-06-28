30b05c21208a62d4f520e9647f2d670445b321da
darbyjack <admin@glaremasters.me>
2018-06-28 12:41:58 -0500

    Started working on setting the handler for yaml storage
    Took 23 seconds

--- src/main/java/me/glaremasters/guilds/database/DatabaseProvider.java
+++ src/main/java/me/glaremasters/guilds/database/DatabaseProvider.java
@@ -1,9 +1,13 @@
 package me.glaremasters.guilds.database;
 
+import java.util.HashMap;
+import me.glaremasters.guilds.guild.Guild;
+
 /**
  * Created by GlareMasters on 6/28/2018.
  */
 public interface DatabaseProvider {
 
+    void getGuilds(Callback<HashMap<String, Guild>, Exception> callback);
 
 }
--- src/main/java/me/glaremasters/guilds/database/databases/yaml/YAML.java
+++ src/main/java/me/glaremasters/guilds/database/databases/yaml/YAML.java
@@ -1,7 +1,10 @@
 package me.glaremasters.guilds.database.databases.yaml;
 
+import java.util.HashMap;
 import me.glaremasters.guilds.Guilds;
+import me.glaremasters.guilds.database.Callback;
 import me.glaremasters.guilds.database.DatabaseProvider;
+import me.glaremasters.guilds.guild.Guild;
 
 /**
  * Created by GlareMasters on 6/28/2018.
@@ -12,6 +15,11 @@ public class YAML implements DatabaseProvider {
 
     public YAML(Guilds guilds) {
         this.guilds = guilds;
+    }
+
+    @Override
+    public void getGuilds(Callback<HashMap<String, Guild>, Exception> callback) {
+
     }
 
 }
