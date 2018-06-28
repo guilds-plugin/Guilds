59951c3cbabd78940dc6b8d4c788f84a5b57298c
darbyjack <admin@glaremasters.me>
2018-06-28 12:41:19 -0500

    Created the event handler base for Guilds
    Took 25 seconds

---
+++ src/main/java/me/glaremasters/guilds/api/events/base/GuildEvent.java
@@ -0,0 +1,36 @@
+package me.glaremasters.guilds.api.events.base;
+
+import me.glaremasters.guilds.guild.Guild;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+/**
+ * Created by GlareMasters on 6/28/2018.
+ */
+public class GuildEvent extends PlayerEvent implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private Guild guild;
+    private boolean cancelled;
+
+    public GuildEvent(Player player, Guild guild) {
+        super(player);
+        this.guild = guild;
+        this.cancelled = false;
+    }
+
+    public static HandlerList getHandlerList() { return handlers; }
+
+    @Override
+    public boolean isCancelled() { return cancelled; }
+
+    @Override
+    public void setCancelled(boolean cancelled) { this.cancelled = cancelled; }
+
+    @Override
+    public HandlerList getHandlers() { return handlers; }
+
+}
