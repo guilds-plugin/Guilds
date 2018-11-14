e0186840e87715263b02c7ef17cf4c10f6cf4ea7
darbyjack <admin@glaremasters.me>
2018-11-14 00:00:28 -0600

    Delete old files after zipping

--- src/main/java/me/glaremasters/guilds/database/databases/json/GuildMapDeserializer.java
+++ src/main/java/me/glaremasters/guilds/database/databases/json/GuildMapDeserializer.java
@@ -5,15 +5,19 @@ import com.google.gson.*;
 import me.glaremasters.guilds.Guilds;
 import me.glaremasters.guilds.guild.Guild;
 import me.glaremasters.guilds.utils.Serialization;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.Inventory;
 
-import java.io.File;
+import java.io.*;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by GlareMasters
@@ -60,6 +64,50 @@ public class GuildMapDeserializer implements JsonDeserializer<Map<String, Guild>
             guilds.put(entry.getKey(), context.deserialize(guild, Guild.class));
         });
         Bukkit.getServer().getScheduler().runTaskLater(Guilds.getGuilds(), () -> Guilds.getGuilds().getDatabase().updateGuild(), 120L);
+        if (status.exists() && homes.exists() && tiers.exists() && banks.exists()) {
+
+            String zipFile = "plugins/Guilds/Old-Guild-Data.zip";
+            String[] srcFiles = {
+                    "data/guild-status.yml",
+                    "data/guild-homes.yml",
+                    "data/guild-tiers.yml",
+                    "data/guild-banks.yml"
+            };
+
+
+            try {
+                byte[] buffer = new byte[1024];
+                FileOutputStream fos = new FileOutputStream(zipFile);
+                ZipOutputStream zos = new ZipOutputStream(fos);
+                for (int i = 0; i < srcFiles.length; i++) {
+                    File srcFile = new File(Guilds.getGuilds().getDataFolder(), srcFiles[i]);
+                    FileInputStream fis = new FileInputStream(srcFile);
+                    zos.putNextEntry(new ZipEntry(srcFile.getName()));
+                    int length;
+                    while ((length = fis.read(buffer)) > 0) {
+                        zos.write(buffer, 0, length);
+                    }
+
+                    zos.closeEntry();
+                    fis.close();
+
+                }
+                zos.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Files.delete(status.toPath());
+                Files.delete(homes.toPath());
+                Files.delete(tiers.toPath());
+                Files.delete(banks.toPath());
+                FileUtils.deleteDirectory(new File(Guilds.getGuilds().getDataFolder(), "data/vaults/"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
         return guilds;
     }
 
