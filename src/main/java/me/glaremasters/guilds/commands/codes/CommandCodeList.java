ae7abff8ab22e35bd51f50cc5f260f6655b9bcc3
darbyjack <admin@glaremasters.me>
2019-04-18 20:05:47 -0500

    Cleaned up some un-needed code in these classes.

--- src/main/java/me/glaremasters/guilds/commands/codes/CommandCodeList.java
+++ src/main/java/me/glaremasters/guilds/commands/codes/CommandCodeList.java
@@ -25,12 +25,14 @@
 package me.glaremasters.guilds.commands.codes;
 
 import ch.jalu.configme.SettingsManager;
+import co.aikar.commands.ACFUtil;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Subcommand;
 import lombok.AllArgsConstructor;
+import me.glaremasters.guilds.exceptions.ExpectationNotMet;
 import me.glaremasters.guilds.messages.Messages;
 import me.glaremasters.guilds.configuration.sections.CodeSettings;
 import me.glaremasters.guilds.guild.Guild;
@@ -58,12 +60,12 @@ public class CommandCodeList extends BaseCommand {
     @CommandPermission(Constants.CODE_PERM + "list")
     public void execute(Player player, Guild guild) {
 
-        if (guild.getCodes().isEmpty()) {
-            getCurrentCommandIssuer().sendInfo(Messages.CODES__EMPTY);
-            return;
-        }
+        if (guild.getCodes().isEmpty())
+            ACFUtil.sneaky(new ExpectationNotMet(Messages.CODES__EMPTY));
 
         getCurrentCommandIssuer().sendInfo(Messages.CODES__LIST_HEADER);
+
+
         if (settingsManager.getProperty(CodeSettings.LIST_INACTIVE_CODES)) {
             guild.getCodes().forEach(c -> getCurrentCommandIssuer().sendInfo(Messages.CODES__LIST_ITEM,
                     "{code}", c.getId(),
