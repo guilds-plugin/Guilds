4aedbc8b2172450b83c539b13882f342fce5991f
darbyjack <admin@glaremasters.me>
2019-05-13 16:25:57 -0500

    Added documentation to itembuilder class.

--- src/main/java/me/glaremasters/guilds/utils/ItemBuilder.java
+++ src/main/java/me/glaremasters/guilds/utils/ItemBuilder.java
@@ -22,16 +22,28 @@ public class ItemBuilder {
     private final ItemStack item;
     private final ItemMeta meta;
 
+    /**
+     * Constructor to build an item
+     * @param item the item to use
+     */
     public ItemBuilder(@NotNull ItemStack item) {
         this.item = item;
         meta = item.getItemMeta();
         Validate.notNull(meta, "Item/material must have ItemMeta");
     }
 
+    /**
+     * Constructor to make an item
+     * @param material the material of the item
+     */
     public ItemBuilder(@NotNull Material material) {
         this(new ItemStack(material));
     }
 
+    /**
+     * Build an item
+     * @return itemstack
+     */
     @NotNull
     @Contract(pure = true)
     public ItemStack build() {
@@ -39,48 +51,91 @@ public class ItemBuilder {
         return item;
     }
 
+    /**
+     * Set the amount of an item
+     * @param amount the amount of an item
+     * @return amounted item
+     */
     @NotNull
     public ItemBuilder setAmount(int amount) {
         item.setAmount(amount);
         return this;
     }
 
+    /**
+     * Set the name of an item
+     * @param name the name of the item
+     * @return item with name
+     */
     @NotNull
     public ItemBuilder setName(@NotNull String name) {
         meta.setDisplayName(name);
         return this;
     }
 
+    /**
+     * Set the lore of an item
+     * @param lore the lore to add
+     * @return item with lore
+     */
     @NotNull
     public ItemBuilder setLore(@NotNull List<String> lore) {
         meta.setLore(lore);
         return this;
     }
 
+    /**
+     * Set the lore of an item
+     * @param lore the lore to add
+     * @return item with lore
+     */
     @NotNull
     public ItemBuilder setLore(@NotNull String... lore) {
         meta.setLore(Arrays.asList(lore));
         return this;
     }
 
+    /**
+     * Add enachants to an item
+     * @param enchantment the enchant to add
+     * @param level the level of the enchant
+     * @return the item with enchantments
+     */
     @NotNull
     public ItemBuilder addEnchantment(@NotNull Enchantment enchantment, int level) {
         meta.addEnchant(enchantment, level, true);
         return this;
     }
 
+    /**
+     * Add a custom item flag to itembuilder
+     * @param flags the flags to add
+     * @return itembuilder item with custom flags
+     */
     @NotNull
     public ItemBuilder addItemFlags(@NotNull ItemFlag... flags) {
         meta.addItemFlags(flags);
         return this;
     }
 
+    /**
+     * Sets an itembuilder item to be unbreakable
+     * @param unbreakable the option
+     * @return itembuilder with unbreakable option
+     */
     @NotNull
     public ItemBuilder setUnbreakable(boolean unbreakable) {
         meta.setUnbreakable(unbreakable);
         return this;
     }
 
+    /**
+     * Apply custom meta to an item
+     * @param type the kind of meta
+     * @param applier the type to apply
+     * @param <T> the type to add
+     * @return itembuilder with custom meta
+     */
     @NotNull
     public <T extends ItemMeta> ItemBuilder applyCustomMeta(@NotNull Class<T> type, @NotNull Consumer<T> applier) {
         applier.accept(type.cast(meta));
