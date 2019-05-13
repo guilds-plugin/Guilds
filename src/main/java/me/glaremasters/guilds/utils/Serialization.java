3a6b391f449a0971b6d8e367263bd8448f14edf0
darbyjack <admin@glaremasters.me>
2019-05-13 14:07:39 -0500

    Allow colorized vault names.

--- src/main/java/me/glaremasters/guilds/utils/Serialization.java
+++ src/main/java/me/glaremasters/guilds/utils/Serialization.java
@@ -25,6 +25,7 @@
 package me.glaremasters.guilds.utils;
 
 import ch.jalu.configme.SettingsManager;
+import co.aikar.commands.ACFBukkitUtil;
 import com.dumptruckman.bukkit.configuration.json.JsonConfiguration;
 import me.glaremasters.guilds.configuration.sections.GuildVaultSettings;
 import org.bukkit.Bukkit;
@@ -86,7 +87,7 @@ public class Serialization {
             json.loadFromString(jsons);
 
             int size = json.getInt("size", 54);
-            title = settingsManager.getProperty(GuildVaultSettings.VAULT_NAME);
+            title = ACFBukkitUtil.color(settingsManager.getProperty(GuildVaultSettings.VAULT_NAME));
 
             Inventory inventory = Bukkit.createInventory(null, size, title);
             Map<String, Object> items = json.getConfigurationSection("items").getValues(false);
