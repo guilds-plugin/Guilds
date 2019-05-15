8348437f1c4db60ee063d97d7d0295fd3790ecf8
darbyjack <admin@glaremasters.me>
2019-05-14 21:07:04 -0500

    Added support for {guild-tier-name} in the list config.

--- src/main/java/me/glaremasters/guilds/guis/ListGUI.java
+++ src/main/java/me/glaremasters/guilds/guis/ListGUI.java
@@ -121,7 +121,8 @@ public class ListGUI {
                     .replace("{guild-status}", guild.getStatus().name())
                     .replace("{guild-tier}", String.valueOf(guild.getTier().getLevel()))
                     .replace("{guild-balance}", String.valueOf(guild.getBalance()))
-                    .replace("{guild-member-count}", String.valueOf(guild.getSize())))));
+                    .replace("{guild-member-count}", String.valueOf(guild.getSize())
+                    .replace("{guild-tier-name}", guildHandler.getGuildTier(guild.getTier().getLevel()).getName())))));
         return updated;
     }
 
