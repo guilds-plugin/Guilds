7c35ea07f94b7c317901ee1337fdbc34dcd3b0ac
darbyjack <admin@glaremasters.me>
2019-05-29 09:45:47 -0500

    Added in admin unclaim command.

---
+++ src/main/java/me/glaremasters/guilds/commands/admin/CommandAdminUnclaim.java
@@ -0,0 +1,84 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Glare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.glaremasters.guilds.commands.admin;
+
+import co.aikar.commands.ACFUtil;
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandCompletion;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Dependency;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.Single;
+import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.Syntax;
+import co.aikar.commands.annotation.Values;
+import me.glaremasters.guilds.exceptions.ExpectationNotMet;
+import me.glaremasters.guilds.guild.Guild;
+import me.glaremasters.guilds.messages.Messages;
+import me.glaremasters.guilds.utils.ClaimUtils;
+import me.glaremasters.guilds.utils.Constants;
+import ch.jalu.configme.SettingsManager;
+import me.glaremasters.guilds.guild.GuildHandler;
+import org.bukkit.entity.Player;
+import org.codemc.worldguardwrapper.WorldGuardWrapper;
+
+/**
+ * Created by Glare
+ * Date: 5/29/2019
+ * Time: 9:34 AM
+ */
+@CommandAlias(Constants.ROOT_ALIAS)
+public class CommandAdminUnclaim extends BaseCommand {
+
+    @Dependency private GuildHandler guildHandler;
+    @Dependency private SettingsManager settingsManager;
+
+    @Subcommand("admin unclaim")
+    @Description("{@@descriptions.admin-unclaim}")
+    @CommandPermission(Constants.ADMIN_PERM)
+    @CommandCompletion("@guilds")
+    @Syntax("<name>")
+    public void execute(Player player, @Values("@guilds") @Single String name) {
+        Guild guild = guildHandler.getGuild(name);
+
+        if (guild == null)
+            ACFUtil.sneaky(new ExpectationNotMet(Messages.ERROR__GUILD_NO_EXIST));
+
+        if (!ClaimUtils.isEnable(settingsManager))
+            ACFUtil.sneaky(new ExpectationNotMet(Messages.CLAIM__HOOK_DISABLED));
+
+        WorldGuardWrapper wrapper = WorldGuardWrapper.getInstance();
+
+        if (!ClaimUtils.checkAlreadyExist(wrapper, player, guild)) {
+            getCurrentCommandManager().getCommandIssuer(player).sendInfo(Messages.UNCLAIM__NOT_FOUND);
+            return;
+        }
+
+        ClaimUtils.removeClaim(wrapper, guild, player);
+        getCurrentCommandIssuer().sendInfo(Messages.UNCLAIM__SUCCESS);
+    }
+
+}
