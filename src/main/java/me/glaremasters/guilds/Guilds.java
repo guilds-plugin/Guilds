20e6abb142b62db74ff1f445d08ca2420219e545
darbyjack <admin@glaremasters.me>
2019-06-07 15:07:52 -0500

    Started working on a switch to a new library loader.

--- src/main/java/me/glaremasters/guilds/Guilds.java
+++ src/main/java/me/glaremasters/guilds/Guilds.java
@@ -42,9 +42,8 @@ import me.glaremasters.guilds.cooldowns.CooldownHandler;
 import me.glaremasters.guilds.database.DatabaseProvider;
 import me.glaremasters.guilds.database.cooldowns.CooldownsProvider;
 import me.glaremasters.guilds.database.providers.JsonProvider;
-import me.glaremasters.guilds.dependency.DependencyLoader;
-import me.glaremasters.guilds.dependency.MavenLibraries;
-import me.glaremasters.guilds.dependency.MavenLibrary;
+import me.glaremasters.guilds.dependency.Library;
+import me.glaremasters.guilds.dependency.LibraryLoader;
 import me.glaremasters.guilds.guild.Guild;
 import me.glaremasters.guilds.guild.GuildCode;
 import me.glaremasters.guilds.guild.GuildHandler;
@@ -94,21 +93,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-@MavenLibraries(
-        value = {
-                @MavenLibrary(groupId = "co.aikar", artifactId = "taskchain-core", version = "3.7.2", repo = "https://repo.glaremasters.me/repository/public/"),
-                @MavenLibrary(groupId = "co.aikar", artifactId = "taskchain-bukkit", version = "3.7.2", repo = "https://repo.glaremasters.me/repository/public/"),
-                @MavenLibrary(groupId = "net.lingala.zip4j", artifactId = "zip4j", version = "1.3.2"),
-                @MavenLibrary(groupId = "commons-io", artifactId = "commons-io", version = "2.6"),
-                @MavenLibrary(groupId = "com.github.stefvanschie.inventoryframework", artifactId = "IF", version = "0.3.1", repo = "https://repo.glaremasters.me/repository/public/"),
-                @MavenLibrary(groupId = "com.dumptruckman.minecraft", artifactId = "JsonConfiguration", version = "1.1", repo = "https://repo.glaremasters.me/repository/public/"),
-                @MavenLibrary(groupId = "net.minidev", artifactId = "json-smart", version = "1.1.1", repo = "https://repo.glaremasters.me/repository/public/"),
-                @MavenLibrary(groupId = "org.codemc.worldguardwrapper", artifactId = "worldguardwrapper", version = "1.1.6-SNAPSHOT", repo = "https://repo.glaremasters.me/repository/public/"),
-                @MavenLibrary(groupId = "org.javassist", artifactId = "javassist", version = "3.21.0-GA"),
-                @MavenLibrary(groupId = "org.reflections", artifactId = "reflections", version = "0.9.11"),
-                @MavenLibrary(groupId = "ch.jalu", artifactId = "configme", version = "1.1.0", repo = "https://repo.glaremasters.me/repository/public/")
-        }
-)
 @Getter
 public final class Guilds extends JavaPlugin {
 
@@ -144,11 +128,32 @@ public final class Guilds extends JavaPlugin {
     @Override
     public void onLoad() {
         try {
-            DependencyLoader.loadAll(getClass());
+            LibraryLoader loader = new LibraryLoader(this);
+            loader.addRepository("https://repo.glaremasters.me/repository/public/");
+            loadDepLibs(loader);
             successfulLoad = true;
         } catch (RuntimeException ex) {
             ex.printStackTrace();
         }
+    }
+
+    /**
+     * Load all the dependencies for the plugin
+     * @param loader the loader to add to
+     */
+    private void loadDepLibs(LibraryLoader loader) {
+        loader.loadLibrary(Library.builder().groupId("commons-io").artifactId("commons-io").version("2.6").checksum("+HfTBGYKwqFC84ZbrfyXHex+1zx0fH+NXS9ROcpzZRM=").relocate("org.apache.commons", "me.glaremasters.guilds.libs.commons-io").build());
+        loader.loadLibrary(Library.builder().groupId("co.aikar").artifactId("taskchain-core").version("3.7.2").checksum("OpSCCN+7v6gqFpsU/LUNOOXzjImwjyE2ShHZ5xFUj/Q=").build());
+        loader.loadLibrary(Library.builder().groupId("co.aikar").artifactId("taskchain-bukkit").version("3.7.2").checksum("B/O3+zWGalLs8otAr8tdNnIc/39FDRh6tN5qvNgfEaI=").build());
+        loader.loadLibrary(Library.builder().groupId("net.lingala.zip4j").artifactId("zip4j").version("1.3.2").checksum("xnCY1DDFdDEUMnKOvUx8RWcvnM9cZHAutq+4gWwirQg=").build());
+        loader.loadLibrary(Library.builder().groupId("com.github.stefvanschie.inventoryframework").artifactId("IF").version("0.3.1").checksum("MOPOPYQSpI3jqFrhQkpTABdO2JpoN4kNqFQTxq7KB+E=").build());
+        loader.loadLibrary(Library.builder().groupId("com.dumptruckman.minecraft").artifactId("JsonConfiguration").version("1.1").checksum("aEEn9nIShT4mvJlF538Mnv+hbP/Yv17ANGchaaBoyCw=").build());
+        loader.loadLibrary(Library.builder().groupId("net.minidev").artifactId("json-smart").version("1.1.1").checksum("zr2iXDGRqkQWc8Q9elqVZ6pdhqEBAa6RWohckLzuh3E=").build());
+        loader.loadLibrary(Library.builder().groupId("org.codemc.worldguardwrapper").artifactId("worldguardwrapper").version("1.1.6-SNAPSHOT").checksum("G023FrJyvpmZxVWeXcGUCBipNB3BSA3rKcKPnCP7Sac=").build());
+        loader.loadLibrary(Library.builder().groupId("org.javassist").artifactId("javassist").version("3.21.0-GA").checksum("eqWeAx+UGYSvB9rMbKhebcm9OkhemqJJTLwDTvoSJdA=").build());
+        loader.loadLibrary(Library.builder().groupId("org.reflections").artifactId("reflections").version("0.9.11").checksum("zKiEKPiokZ34hRBYM9Rf8HvSb5hflu5VaQVRIWtYtKE=").build());
+        loader.loadLibrary(Library.builder().groupId("ch.jalu").artifactId("configme").version("1.1.0").checksum("c3EUKZSs/xPSHwn/K0KMf9hTbN0ijRXyIBtOg5PxUnI=").build());
+        loader.loadLibrary(Library.builder().groupId("com.google.guava").artifactId("guava").version("21.0").checksum("lyE5cYq8ikiT+njLqM97LJA/Ncl6r0T6MDGwZplItIA=").relocate("com.google.common", "me.glaremasters.guilds.libs.guava").build());
     }
 
     /**
