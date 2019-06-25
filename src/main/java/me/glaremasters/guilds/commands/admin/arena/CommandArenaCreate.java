774c498836a9c06a49928babd9965e46c7afb531
darbyjack <admin@glaremasters.me>
2019-06-25 15:41:47 -0500

    Started making barebone classes for arena.

---
+++ src/main/java/me/glaremasters/guilds/commands/admin/arena/CommandArenaCreate.java
@@ -0,0 +1,6 @@
+package me.glaremasters.guilds.commands.admin.arena;
+
+import co.aikar.commands.BaseCommand;
+
+public class CommandArenaCreate extends BaseCommand {
+}
---
+++ src/main/java/me/glaremasters/guilds/commands/admin/arena/CommandArenaDefender.java
@@ -0,0 +1,6 @@
+package me.glaremasters.guilds.commands.admin.arena;
+
+import co.aikar.commands.BaseCommand;
+
+public class CommandArenaDefender extends BaseCommand {
+}
