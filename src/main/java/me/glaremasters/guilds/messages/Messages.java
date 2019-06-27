dfcb7db2602bc3d89ff9464f519da886c0572781
darbyjack <admin@glaremasters.me>
2019-06-27 12:06:55 -0500

    Added in command to list the arenas on server.

--- src/main/java/me/glaremasters/guilds/messages/Messages.java
+++ src/main/java/me/glaremasters/guilds/messages/Messages.java
@@ -124,7 +124,7 @@ public enum Messages implements MessageKeyProvider {
     MOTD__SUCCESS, MOTD__REMOVE, MOTD__MOTD, MOTD__NOT_SET,
 
     ARENA__CREATED, ARENA__NO_EXIST, ARENA__CHALLENGER_SET, ARENA__DEFENDER_SET,
-    ARENA__DELETED;
+    ARENA__DELETED, ARENA__LIST, ARENA__LIST_EMPTY;
 
     /**
      * Message keys that grab from the config to send messages
