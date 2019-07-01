737642fafda5bf351733a7a97b7c5cd71dbce2c9
darbyjack <admin@glaremasters.me>
2019-06-30 21:02:27 -0500

    Fixed some changes. Added in messages on successful challenge sent.

--- src/main/java/me/glaremasters/guilds/commands/war/CommandChallenge.java
+++ src/main/java/me/glaremasters/guilds/commands/war/CommandChallenge.java
@@ -1,5 +1,6 @@
 package me.glaremasters.guilds.commands.war;
 
+import ch.jalu.configme.SettingsManager;
 import co.aikar.commands.ACFUtil;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
@@ -12,25 +13,30 @@ import co.aikar.commands.annotation.Subcommand;
 import co.aikar.commands.annotation.Syntax;
 import co.aikar.commands.annotation.Values;
 import me.glaremasters.guilds.arena.ArenaHandler;
+import me.glaremasters.guilds.configuration.sections.WarSettings;
 import me.glaremasters.guilds.exceptions.ExpectationNotMet;
 import me.glaremasters.guilds.exceptions.InvalidPermissionException;
 import me.glaremasters.guilds.guild.Guild;
+import me.glaremasters.guilds.guild.GuildChallenge;
 import me.glaremasters.guilds.guild.GuildHandler;
 import me.glaremasters.guilds.guild.GuildRole;
 import me.glaremasters.guilds.messages.Messages;
 import me.glaremasters.guilds.utils.Constants;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 @CommandAlias(Constants.ROOT_ALIAS)
 public class CommandChallenge extends BaseCommand {
 
     @Dependency private GuildHandler guildHandler;
     @Dependency private ArenaHandler arenaHandler;
+    @Dependency private SettingsManager settingsManager;
 
-    @Subcommand("challenge")
-    @Description("{@@descriptions.challenge}")
+    @Subcommand("challenges")
+    @Description("{@@descriptions.challenges}")
     @Syntax("<guild>")
-    @CommandPermission(Constants.WAR_PERM + "challenge")
+    @CommandPermission(Constants.WAR_PERM + "challenges")
     @CommandCompletion("@guilds")
     public void execute(Player player, Guild guild, GuildRole role, @Values("@guilds") @Single String target) {
         if (!role.isInitiateWar())
@@ -47,9 +53,24 @@ public class CommandChallenge extends BaseCommand {
         if (targetGuild == null)
             ACFUtil.sneaky(new ExpectationNotMet(Messages.ERROR__GUILD_NO_EXIST));
 
-        // Check for online defenders to accept challenge
+        // Check for online defenders to accept challenges
         if (guildHandler.getOnlineDefenders(targetGuild).isEmpty())
             ACFUtil.sneaky(new ExpectationNotMet(Messages.WAR__NO_DEFENDERS));
+
+        // Create the new guild challenge
+        GuildChallenge challenge = new GuildChallenge(UUID.randomUUID(), System.currentTimeMillis(), guild.getId(), targetGuild.getId(), false);
+
+        // Add the new challenge to the handler
+        guildHandler.getChallenges().add(challenge);
+
+        // Set an int to the amount of time allowed to accept it an invite
+        int acceptTime = settingsManager.getProperty(WarSettings.ACCEPT_TIME);
+
+        // Send message to challenger saying that they've sent the challenge.
+        getCurrentCommandIssuer().sendInfo(Messages.WAR__CHALLENGE_SENT, "{guild}", targetGuild.getName(), "{amount}", String.valueOf(acceptTime));
+
+        // Send message to defending guild
+        guildHandler.pingOnlineDefenders(targetGuild, getCurrentCommandManager(), guild.getName(), acceptTime);
     }
 
 }
