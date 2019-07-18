8b523b18993b11ad8763d8baad9dae1feb3f3d4a
darbyjack <admin@glaremasters.me>
2019-07-18 13:33:14 -0500

    Handle removing a player from the challenge when a player leaves.

--- src/main/java/me/glaremasters/guilds/listeners/ArenaListener.java
+++ src/main/java/me/glaremasters/guilds/listeners/ArenaListener.java
@@ -1,4 +1,25 @@
 package me.glaremasters.guilds.listeners;
 
-public class ArenaListener {
+import lombok.AllArgsConstructor;
+import me.glaremasters.guilds.challenges.ChallengeHandler;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+@AllArgsConstructor
+public class ArenaListener implements Listener {
+
+    private ChallengeHandler challengeHandler;
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        // Check if they are part of an active challenge
+        if (challengeHandler.getChallenge(player) != null) {
+            // Remove the player from the alive players
+            challengeHandler.removePlayer(player);
+        }
+    }
+
 }
