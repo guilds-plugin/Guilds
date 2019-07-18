b6518993960367e0ceea39b1b8223eff602f2aef
darbyjack <admin@glaremasters.me>
2019-07-18 16:03:44 -0500

    Detailed what saves in the json file.

--- src/main/java/me/glaremasters/guilds/guild/GuildChallenge.java
+++ src/main/java/me/glaremasters/guilds/guild/GuildChallenge.java
@@ -1,10 +1,13 @@
 package me.glaremasters.guilds.guild;
 
+import com.google.gson.annotations.JsonAdapter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import me.glaremasters.guilds.arena.Arena;
+import me.glaremasters.guilds.challenges.adapters.WarArenaChallengeAdapter;
+import me.glaremasters.guilds.challenges.adapters.WarGuildChallengeAdapater;
 
 import java.util.List;
 import java.util.Map;
@@ -18,18 +21,21 @@ public class GuildChallenge {
 
     private UUID id;
     private long initiateTime;
+    @JsonAdapter(WarGuildChallengeAdapater.class)
     private Guild challenger;
+    @JsonAdapter(WarGuildChallengeAdapater.class)
     private Guild defender;
-    private boolean accepted;
-    private boolean joinble;
-    private boolean started;
-    private int minPlayersPerSide;
-    private int maxPlayersPerSide;
+    private transient boolean accepted;
+    private transient boolean joinble;
+    private transient boolean started;
+    private transient int minPlayersPerSide;
+    private transient int maxPlayersPerSide;
     private List<UUID> challengePlayers;
     private List<UUID> defendPlayers;
+    @JsonAdapter(WarArenaChallengeAdapter.class)
     private Arena arena;
     private String winner;
 
-    private Map<UUID, String> aliveChallengers;
-    private Map<UUID, String> aliveDefenders;
+    private transient Map<UUID, String> aliveChallengers;
+    private transient Map<UUID, String> aliveDefenders;
 }
