30c85876ef7228c94442b6b1b3ea9ce7913de4f7
darbyjack <admin@glaremasters.me>
2019-08-17 13:57:49 -0500

    Specified what isn't affected in SQLite for database management

--- src/main/java/me/glaremasters/guilds/configuration/sections/StorageSettings.java
+++ src/main/java/me/glaremasters/guilds/configuration/sections/StorageSettings.java
@@ -17,15 +17,15 @@ public class StorageSettings implements SettingsHolder {
     public static final Property<Integer> SAVE_INTERVAL =
             newProperty("storage.save-interval", 1);
 
-    @Comment("Define the address for the database.")
+    @Comment("Define the address for the database. (Doesn't apply to SQLite)")
     public static final Property<String> SQL_HOST =
             newProperty("storage.sql.host", "localhost");
 
-    @Comment("Define the port for the database.")
+    @Comment("Define the port for the database. (Doesn't apply to SQLite)")
     public static final Property<String> SQL_PORT =
             newProperty("storage.sql.port", "3306");
 
-    @Comment({"The name of the database to store data in.", "This must be already created!"})
+    @Comment({"The name of the database to store data in.", "This must be already created! (Doesn't apply to SQLite)"})
     public static final Property<String> SQL_DATABASE =
             newProperty("storage.sql.database", "guilds");
 
@@ -33,29 +33,29 @@ public class StorageSettings implements SettingsHolder {
     public static final Property<String> SQL_TABLE_PREFIX =
             newProperty("storage.sql.table-prefix", "guilds_");
 
-    @Comment("Define the credentials for the database.")
+    @Comment("Define the credentials for the database. (Doesn't apply to SQLite)")
     public static final Property<String> SQL_USERNAME =
-            newProperty("storage.sql.username", "root");
+            newProperty("storage.sql.username (Doesn't apply to SQLite)", "root");
     public static final Property<String> SQL_PASSWORD =
             newProperty("storage.sql.password", "");
 
-    @Comment("Sets whether or not to use SSL for the remote SQL database connection")
+    @Comment("Sets whether or not to use SSL for the remote SQL database connection (Doesn't apply to SQLite)")
     public static final Property<Boolean> SQL_ENABLE_SSL =
             newProperty("storage.sql.enable-ssl", false);
 
-    @Comment({"Sets the maximum size of the SQL connection pool.", "This value will determine the maximum number of connections maintained."})
+    @Comment({"Sets the maximum size of the SQL connection pool.", "This value will determine the maximum number of connections maintained. (Doesn't apply to SQLite)"})
     public static final Property<Integer> SQL_POOL_SIZE =
             newProperty("storage.sql.pool.maximum-pool-size", 10);
 
-    @Comment({"Sets the minimum number of idle connections that the pool will maintain.", "For maximum performance keep this value the same as 'maximum-pool-size'"})
+    @Comment({"Sets the minimum number of idle connections that the pool will maintain.", "For maximum performance keep this value the same as 'maximum-pool-size' (Doesn't apply to SQLite)"})
     public static final Property<Integer> SQL_POOL_IDLE =
             newProperty("storage.sql.pool.minimum-idle", 10);
 
-    @Comment({"Sets the maximum lifetime of a connection in the pool in milliseconds."})
+    @Comment({"Sets the maximum lifetime of a connection in the pool in milliseconds. (Doesn't apply to SQLite)"})
     public static final Property<Integer> SQL_POOL_LIFETIME =
             newProperty("storage.sql.pool.maximum-lifetime", 1800000);
 
-    @Comment("Sets the maximum number of milliseconds for a connection in the pool before timing out.")
+    @Comment("Sets the maximum number of milliseconds for a connection in the pool before timing out. (Doesn't apply to SQLite)")
     public static final Property<Integer> SQL_POOL_TIMEOUT =
             newProperty("storage.sql.pool.connection-timeout", 5000);
 
