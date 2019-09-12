570b1b061a2524930b96d6973ddb9e6282162538
darbyjack <admin@glaremasters.me>
2019-09-12 14:40:26 -0500

    Moved UnclaimAll to console command

--- src/main/java/me/glaremasters/guilds/commands/console/CommandUnclaimAll.java
+++ src/main/java/me/glaremasters/guilds/commands/console/CommandUnclaimAll.java
@@ -9,6 +9,8 @@ import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Dependency;
 import co.aikar.commands.annotation.Description;
 import co.aikar.commands.annotation.Subcommand;
+import me.glaremasters.guilds.actions.ActionHandler;
+import me.glaremasters.guilds.actions.ConfirmAction;
 import me.glaremasters.guilds.exceptions.ExpectationNotMet;
 import me.glaremasters.guilds.guild.GuildHandler;
 import me.glaremasters.guilds.messages.Messages;
@@ -21,6 +23,7 @@ public class CommandUnclaimAll extends BaseCommand {
 
     @Dependency private GuildHandler guildHandler;
     @Dependency private SettingsManager settingsManager;
+    @Dependency private ActionHandler actionHandler;
 
     @Subcommand("console unclaimall")
     @Description("{@@descriptions.console-unclaim-all}")
@@ -33,13 +36,26 @@ public class CommandUnclaimAll extends BaseCommand {
         if (!ClaimUtils.isEnable(settingsManager))
             ACFUtil.sneaky(new ExpectationNotMet(Messages.CLAIM__HOOK_DISABLED));
 
-        WorldGuardWrapper wrapper = WorldGuardWrapper.getInstance();
+        getCurrentCommandIssuer().sendInfo(Messages.MIGRATE__WARNING);
 
-        guildHandler.getGuilds().forEach(g -> {
-            if (ClaimUtils.checkAlreadyExist(wrapper, g)) {
-                ClaimUtils.removeClaim(wrapper, g);
+        actionHandler.addAction(issuer.getIssuer(), new ConfirmAction() {
+            @Override
+            public void accept() {
+                WorldGuardWrapper wrapper = WorldGuardWrapper.getInstance();
+
+                guildHandler.getGuilds().forEach(g -> {
+                    if (ClaimUtils.checkAlreadyExist(wrapper, g)) {
+                        ClaimUtils.removeClaim(wrapper, g);
+                    }
+                });
+                getCurrentCommandIssuer().sendInfo(Messages.UNCLAIM__ALL_SUCCESS);
+            }
+
+            @Override
+            public void decline() {
+                getCurrentCommandIssuer().sendInfo(Messages.UNCLAIM__ALL_CANCELLED);
+                actionHandler.removeAction(issuer.getIssuer());
             }
         });
-        getCurrentCommandIssuer().sendInfo(Messages.UNCLAIM__ALL_SUCCESS);
     }
 }
