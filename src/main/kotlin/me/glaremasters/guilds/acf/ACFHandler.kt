ab4e0f78000c6eb256a209a8221233d0a7d3c2f9
darbyjack <admin@glaremasters.me>
2020-02-21 10:00:17 -0600

    Updated guava

--- src/main/kotlin/me/glaremasters/guilds/acf/ACFHandler.kt
+++ src/main/kotlin/me/glaremasters/guilds/acf/ACFHandler.kt
@@ -147,7 +147,11 @@ class ACFHandler(private val plugin: Guilds, private val commandManager: PaperCo
 
     private fun loadCommands() {
         val classes = ClassPath.from(this.javaClass.classLoader).getTopLevelClassesRecursive("me.glaremasters.guilds.commands")
-        classes.map(ClassPath.ClassInfo::load).filter(BaseCommand::class.java::isAssignableFrom).map(Class<*>::newInstance).filterIsInstance<BaseCommand>().forEach(commandManager::registerCommand)
+        classes.map(ClassPath.ClassInfo::load)
+                .filter(BaseCommand::class.java::isAssignableFrom)
+                .map(Class<*>::newInstance)
+                .filterIsInstance<BaseCommand>()
+                .forEach(commandManager::registerCommand)
     }
 
     private fun loadDI() {
