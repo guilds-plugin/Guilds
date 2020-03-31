82ab6b1de0f2300fc4dc477c8f456c396302c14e
darbyjack <admin@glaremasters.me>
2020-03-31 16:12:32 -0500

    Converted vault gui

--- src/main/kotlin/me/glaremasters/guilds/guis/VaultGUI.kt
+++ src/main/kotlin/me/glaremasters/guilds/guis/VaultGUI.kt
@@ -25,48 +25,33 @@
 package me.glaremasters.guilds.guis
 
 import ch.jalu.configme.SettingsManager
-import co.aikar.commands.PaperCommandManager
-import com.github.stefvanschie.inventoryframework.Gui
-import com.github.stefvanschie.inventoryframework.GuiItem
-import com.github.stefvanschie.inventoryframework.pane.OutlinePane
-import com.github.stefvanschie.inventoryframework.pane.Pane
 import me.glaremasters.guilds.Guilds
 import me.glaremasters.guilds.configuration.sections.VaultPickerSettings
 import me.glaremasters.guilds.guild.Guild
 import me.glaremasters.guilds.guild.GuildHandler
-import me.glaremasters.guilds.utils.GuiBuilder
+import me.glaremasters.guilds.utils.GuiBuilderTwo
 import me.glaremasters.guilds.utils.GuiUtils
 import me.glaremasters.guilds.utils.StringUtils
+import me.mattstudios.mfgui.gui.guis.Gui
+import me.mattstudios.mfgui.gui.guis.GuiItem
 import org.bukkit.entity.Player
-import org.bukkit.event.inventory.InventoryClickEvent
-import java.util.function.Consumer
 
 class VaultGUI(private val guilds: Guilds, private val settingsManager: SettingsManager, private val guildHandler: GuildHandler) {
 
-    fun getVaultGUI(guild: Guild, player: Player, commandManager: PaperCommandManager): Gui {
+    fun getVaultGUI(guild: Guild, player: Player): Gui {
         val name = settingsManager.getProperty(VaultPickerSettings.GUI_NAME).replace("{name}", guild.name)
         val rows = settingsManager.getProperty(VaultPickerSettings.GUI_SIZE)
-        val gui = GuiBuilder(guilds).setName(name).setRows(rows).addBackground(9, rows).blockGlobalClick().build()
+        val gui = GuiBuilderTwo(guilds).setName(name).setRows(rows).build()
 
         // Prevent players from being able to items into the GUIs
-        gui.setOnOutsideClick { event: InventoryClickEvent ->
+        gui.setOutsideClickAction { event ->
             event.isCancelled = true
             guilds.guiHandler.infoGUI.getInfoGUI(guild, player).show(event.whoClicked)
         }
 
-        // Create the pane for the main items
-        val foregroundPane = OutlinePane(0, 0, 9, settingsManager.getProperty(VaultPickerSettings.GUI_SIZE), Pane.Priority.NORMAL)
+        addItems(gui, guild, player)
 
-        // Add the items to the foreground pane
-        createForegroundItems(foregroundPane, guild, player)
-
-        // Set it back to 0
         num = 0
-
-        // Add the foreground pane to the GUI
-        gui.addPane(foregroundPane)
-
-        // Return the create GUI object
         return gui
     }
 
@@ -76,26 +61,24 @@ class VaultGUI(private val guilds: Guilds, private val settingsManager: Settings
      * @param pane  the pane to be added to
      * @param guild the guild of the player
      */
-    private fun createForegroundItems(pane: OutlinePane, guild: Guild, player: Player) {
+    private fun addItems(gui: Gui, guild: Guild, player: Player) {
         val max = guildHandler.getGuildTier(guild.tier.level).vaultAmount
         for (i in 0 until max) {
             val status = if (guildHandler.hasVaultUnlocked(i + 1, guild)) settingsManager.getProperty(VaultPickerSettings.PICKER_UNLOCKED) else settingsManager.getProperty(VaultPickerSettings.PICKER_LOCKED)
 
             val lore = settingsManager.getProperty(VaultPickerSettings.PICKER_LORE)
-            val updated: MutableList<String> = ArrayList()
+            val updated = mutableListOf<String>()
 
-            lore.forEach(Consumer { line: String ->
+            lore.forEach { line ->
                 updated.add(StringUtils.color(line
                         .replace("{number}", (num + 1).toString())
                         .replace("{status}", status)
                 ))
-            })
+            }
 
-            pane.addItem(GuiItem(GuiUtils.createItem(
-                    settingsManager.getProperty(VaultPickerSettings.PICKER_MATERIAL),
-                    settingsManager.getProperty(VaultPickerSettings.PICKER_NAME),
-                    updated
-            ), Consumer { event: InventoryClickEvent ->
+            val item = GuiItem(GuiUtils.createItem(settingsManager.getProperty(VaultPickerSettings.PICKER_MATERIAL), settingsManager.getProperty(VaultPickerSettings.PICKER_NAME), updated))
+
+            item.setAction { event ->
                 event.isCancelled = true
                 try {
                     guildHandler.getGuildVault(guild, event.rawSlot + 1)
@@ -104,7 +87,9 @@ class VaultGUI(private val guilds: Guilds, private val settingsManager: Settings
                 }
                 player.openInventory(guildHandler.getGuildVault(guild, event.rawSlot + 1))
                 guildHandler.openedVault.add(player)
-            }))
+            }
+
+            gui.addItem(item)
             num++
         }
     }
