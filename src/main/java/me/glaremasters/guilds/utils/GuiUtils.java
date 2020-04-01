7f9ef1fa243a5d63a1b9ffb9c8313086ce477b2c
darbyjack <admin@glaremasters.me>
2020-04-01 09:06:35 -0500

    Temp random change material types from problematic ones

--- src/main/java/me/glaremasters/guilds/utils/GuiUtils.java
+++ src/main/java/me/glaremasters/guilds/utils/GuiUtils.java
@@ -25,22 +25,36 @@
 package me.glaremasters.guilds.utils;
 
 import com.cryptomorin.xseries.XMaterial;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 // This is a temp class until I can sort a bunch of shit out
 public class GuiUtils {
 
+    public static Set<Material> problemItems = new HashSet<>(Arrays.asList(Material.valueOf("REDSTONE_TORCH_OFF"), Material.valueOf("BED_BLOCK")));
+    public static List<XMaterial> replacementsItems = new ArrayList<>(Arrays.asList(XMaterial.DIAMOND_AXE, XMaterial.IRON_BLOCK));
+
     public static ItemStack createItem(String material, String name, List<String> lore) {
         Optional<XMaterial> tempMaterial = XMaterial.matchXMaterial(material);
         XMaterial tempCheck = tempMaterial.orElse(XMaterial.GLASS_PANE);
         ItemStack item = tempCheck.parseItem();
+        System.out.println(item);
         if (item == null) {
             item = XMaterial.GLASS_PANE.parseItem();
+        }
+        // Extra check
+        if (problemItems.contains(item.getType())) {
+            item.setType(getRandom().parseMaterial());
         }
         ItemBuilder builder = new ItemBuilder(item);
         builder.setName(StringUtils.color(name));
@@ -49,5 +63,12 @@ public class GuiUtils {
         }
         builder.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
         return builder.build();
+    }
+
+    public static XMaterial getRandom() {
+        int length = replacementsItems.size();
+        Random random = new Random();
+        int choice = random.nextInt(length);
+        return replacementsItems.get(choice);
     }
 }
