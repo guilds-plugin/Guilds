73b94af47bfec9f0bd6e88e582a63c3154b133a4
darbyjack <admin@glaremasters.me>
2020-04-10 13:44:11 -0500

    Converted all admin manage commands to kotlin

---
+++ src/main/java/me/glaremasters/guilds/commands/admin/manage/CommandAdminRemove.kt
@@ -0,0 +1,91 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Glare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.glaremasters.guilds.commands.admin.manage
+
+import ch.jalu.configme.SettingsManager
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Dependency
+import co.aikar.commands.annotation.Description
+import co.aikar.commands.annotation.Flags
+import co.aikar.commands.annotation.Subcommand
+import co.aikar.commands.annotation.Syntax
+import co.aikar.commands.annotation.Values
+import me.glaremasters.guilds.Guilds
+import me.glaremasters.guilds.actions.ActionHandler
+import me.glaremasters.guilds.actions.ConfirmAction
+import me.glaremasters.guilds.api.events.GuildRemoveEvent
+import me.glaremasters.guilds.exceptions.ExpectationNotMet
+import me.glaremasters.guilds.guild.Guild
+import me.glaremasters.guilds.guild.GuildHandler
+import me.glaremasters.guilds.messages.Messages
+import me.glaremasters.guilds.utils.ClaimUtils
+import me.glaremasters.guilds.utils.Constants
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+
+@CommandAlias("%guilds")
+internal class CommandAdminRemove : BaseCommand() {
+    @Dependency lateinit var guilds: Guilds
+    @Dependency lateinit var guildHandler: GuildHandler
+    @Dependency lateinit var actionHandler: ActionHandler
+    @Dependency lateinit var settingsManager: SettingsManager
+
+    @Subcommand("admin remove")
+    @Description("{@@descriptions.admin-remove}")
+    @CommandPermission(Constants.ADMIN_PERM)
+    @CommandCompletion("@guilds")
+    @Syntax("<%syntax>")
+    fun remove(player: Player, @Flags("admin") @Values("@guilds") guild: Guild) {
+        if (guildHandler.isMigrating) {
+            throw ExpectationNotMet(Messages.ERROR__MIGRATING)
+        }
+        val name = guild.name
+
+        currentCommandIssuer.sendInfo(Messages.ADMIN__DELETE_WARNING, "{guild}", name)
+        actionHandler.addAction(player, object : ConfirmAction {
+            override fun accept() {
+                val event = GuildRemoveEvent(player, guild, GuildRemoveEvent.Cause.ADMIN_DELETED)
+                Bukkit.getPluginManager().callEvent(event)
+
+                if (event.isCancelled) {
+                    return
+                }
+
+                ClaimUtils.deleteWithGuild(guild, settingsManager)
+                guild.sendMessage(currentCommandManager, Messages.LEAVE__GUILDMASTER_LEFT, "{player}", guild.guildMaster.name)
+                guildHandler.removeGuild(guild)
+                currentCommandIssuer.sendInfo(Messages.ADMIN__DELETE_SUCCESS, "{guild}", name)
+                actionHandler.removeAction(player)
+            }
+
+            override fun decline() {
+                actionHandler.removeAction(player)
+            }
+        })
+    }
+}
---
+++ src/main/java/me/glaremasters/guilds/commands/admin/manage/CommandAdminRename.kt
@@ -0,0 +1,60 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Glare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.glaremasters.guilds.commands.admin.manage
+
+import co.aikar.commands.BaseCommand
+import co.aikar.commands.annotation.CommandAlias
+import co.aikar.commands.annotation.CommandCompletion
+import co.aikar.commands.annotation.CommandPermission
+import co.aikar.commands.annotation.Dependency
+import co.aikar.commands.annotation.Description
+import co.aikar.commands.annotation.Flags
+import co.aikar.commands.annotation.Single
+import co.aikar.commands.annotation.Subcommand
+import co.aikar.commands.annotation.Syntax
+import co.aikar.commands.annotation.Values
+import me.glaremasters.guilds.Guilds
+import me.glaremasters.guilds.guild.Guild
+import me.glaremasters.guilds.guild.GuildHandler
+import me.glaremasters.guilds.messages.Messages
+import me.glaremasters.guilds.utils.Constants
+import me.glaremasters.guilds.utils.StringUtils
+import org.bukkit.entity.Player
+
+@CommandAlias("%guilds")
+internal class CommandAdminRename : BaseCommand() {
+    @Dependency lateinit var guilds: Guilds
+    @Dependency lateinit var guildHandler: GuildHandler
+
+    @Subcommand("admin rename")
+    @Description("{@@descriptions.admin-prefix}")
+    @CommandPermission(Constants.ADMIN_PERM)
+    @CommandCompletion("@guilds")
+    @Syntax("<%syntax> <new name>")
+    fun rename(player: Player, @Flags("admin") @Values("@guilds") guild: Guild, @Single name: String) {
+        guild.name = StringUtils.color(name)
+        currentCommandIssuer.sendInfo(Messages.RENAME__SUCCESSFUL, "{name}", guild.name)
+    }
+}
