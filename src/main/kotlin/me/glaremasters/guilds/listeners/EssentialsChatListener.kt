3421bfde361a8220d43779eba12cc6d4f64452a9
darbyjack <admin@glaremasters.me>
2020-04-23 18:42:16 -0500

    Converted over EssX listener

---
+++ src/main/kotlin/me/glaremasters/guilds/listeners/EssentialsChatListener.kt
@@ -0,0 +1,62 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 Glare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package me.glaremasters.guilds.listeners
+
+import me.glaremasters.guilds.guild.GuildHandler
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.AsyncPlayerChatEvent
+
+class EssentialsChatListener(private val guildHandler: GuildHandler) : Listener {
+
+    @EventHandler
+    fun AsyncPlayerChatEvent.onChat() {
+        val guild = guildHandler.getGuild(player)
+        var message = format
+
+        if (guild == null) {
+            val regex = "(\\{GUILD(?:.*?)})"
+            val formatted = "(\\\\{GUILD_FORMATTED\\\\})"
+
+            format = message.replace(formatted, guildHandler.getFormattedPlaceholder(player))
+            format = message.replace(regex.toRegex(), "")
+            return
+        }
+
+        message = message
+                .replace("{GUILD}", guild.name)
+                .replace("{GUILD_PREFIX}", guild.prefix)
+                .replace("{GUILD_MASTER}", guild.guildMaster.name.toString())
+                .replace("{GUILD_STATUS}", guild.status.name)
+                .replace("{GUILD_MEMBER_COUNT}", guild.size.toString())
+                .replace("{GUILD_MEMBERS_ONLINE}", guild.onlineMembers.size.toString())
+                .replace("{GUILD_ROLE}", guild.getMember(player.uniqueId).role.name)
+                .replace("{GUILD_FORMATTED}", guildHandler.getFormattedPlaceholder(player))
+                .replace("{GUILD_CHALLENGE_WINS}", guild.guildScore.wins.toString())
+                .replace("{GUILD_CHALLENGE_LOSES}", guild.guildScore.loses.toString())
+
+        format = message
+    }
+}
