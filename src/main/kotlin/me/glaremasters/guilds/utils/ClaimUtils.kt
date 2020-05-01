aca47af1fa16795600e0466968efa4a68f19afdc
darbyjack <admin@glaremasters.me>
2020-05-01 17:19:21 -0500

    Fixed not properly going through all the worlds

--- src/main/kotlin/me/glaremasters/guilds/utils/ClaimUtils.kt
+++ src/main/kotlin/me/glaremasters/guilds/utils/ClaimUtils.kt
@@ -74,11 +74,11 @@ object ClaimUtils {
     fun checkAlreadyExist(wrapper: WorldGuardWrapper, guild: Guild): Boolean {
         for (world in Bukkit.getWorlds()) {
             val tempRegion = wrapper.getRegion(world, getClaimName(guild))
-            return try {
+            try {
                 tempRegion.get().id
-                true
+                return true
             } catch (ex: Exception) {
-                false
+                continue
             }
         }
         return false
