d5d0d898283d91a7ca3f30f9838d37b225aef784
darbyjack <admin@glaremasters.me>
2020-07-19 18:05:47 -0500

    Updated XSeries

--- src/main/java/me/glaremasters/guilds/utils/GuiUtils.java
+++ src/main/java/me/glaremasters/guilds/utils/GuiUtils.java
@@ -39,7 +39,7 @@ import java.util.stream.Collectors;
 // This is a temp class until I can sort a bunch of shit out
 public class GuiUtils {
 
-    public static Set<Material> problemItems = new HashSet<>(Arrays.asList(XMaterial.REDSTONE_TORCH.parseMaterial(), XMaterial.RED_BED.parseMaterial()));
+    public static Set<Material> problemItems = new HashSet<>(Arrays.asList(XMaterial.REDSTONE_TORCH.parseMaterial().get(), XMaterial.RED_BED.parseMaterial().get()));
 
     public static ItemStack createItem(String material, String name, List<String> lore) {
         Optional<XMaterial> tempMaterial = XMaterial.matchXMaterial(material);
@@ -51,7 +51,7 @@ public class GuiUtils {
         // Extra check
         if (problemItems.contains(item.getType())) {
             LoggingUtils.warn("Problematic Material Type Found! Switching to Barrier.");
-            item.setType(XMaterial.BARRIER.parseMaterial());
+            item.setType(XMaterial.BARRIER.parseMaterial().get());
         }
         ItemBuilder builder = new ItemBuilder(item);
         builder.setName(StringUtils.color(name));
