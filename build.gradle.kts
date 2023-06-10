834d1b0638131ccfc5123d638fada165e803530a
darbyjack <admin@glaremasters.me>
2023-06-10 13:46:22 -0500

    chore(deps): bump deps for 1.20 support

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@ import java.net.URL
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.8.20"
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
     id("net.kyori.indra") version "3.0.1"
     id("net.kyori.indra.publishing") version "3.0.1"
     id("net.kyori.indra.license-header") version "3.0.1"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.github.slimjar") version "1.3.0"
     id("xyz.jpenilla.run-paper") version "2.0.1"
-    id("com.github.ben-manes.versions") version "0.46.0"
+    id("com.github.ben-manes.versions") version "0.47.0"
     id("org.jetbrains.dokka") version "1.8.10"
 }
 
@@ -50,12 +50,12 @@ repositories {
 dependencies {
     implementation("io.github.slimjar:slimjar:1.2.7")
     implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
-    implementation("org.bstats:bstats-bukkit:3.0.1")
+    implementation("org.bstats:bstats-bukkit:3.0.2")
     implementation("co.aikar:taskchain-bukkit:3.7.2")
     implementation("org.codemc.worldguardwrapper:worldguardwrapper:1.1.9-SNAPSHOT")
     implementation("ch.jalu:configme:1.3.0")
     implementation("com.dumptruckman.minecraft:JsonConfiguration:1.1")
-    implementation("com.github.cryptomorin:XSeries:9.3.1")
+    implementation("com.github.cryptomorin:XSeries:9.4.0")
     implementation("net.kyori:adventure-platform-bukkit:4.3.0")
     implementation("dev.triumphteam:triumph-gui:3.1.4")
     implementation("com.zaxxer:HikariCP:4.0.3")
@@ -63,7 +63,7 @@ dependencies {
     implementation("org.jdbi:jdbi3-sqlobject:3.8.2")
     implementation("org.mariadb.jdbc:mariadb-java-client:2.7.2")
 
-    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
     compileOnly("net.milkbowl:vault:1.7")
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("me.clip:placeholderapi:2.11.3")
@@ -117,7 +117,7 @@ tasks {
     }
 
     runServer {
-        minecraftVersion("1.19.4")
+        minecraftVersion("1.20")
     }
 
     license {
