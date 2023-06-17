3b4df793feaeb79d5d349c865f2eb83825cba03e
darbyjack <admin@glaremasters.me>
2023-06-17 14:22:39 -0500

    chore: release 3.5.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "me.glaremasters"
-version = "3.5.7.0-SNAPSHOT"
+version = "3.5.7.0"
 
 base {
     archivesBaseName = "Guilds"
@@ -63,7 +63,7 @@ dependencies {
     implementation("org.jdbi:jdbi3-sqlobject:3.8.2")
     implementation("org.mariadb.jdbc:mariadb-java-client:2.7.2")
 
-    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
     compileOnly("net.milkbowl:vault:1.7")
     compileOnly("com.mojang:authlib:1.5.25")
     compileOnly("me.clip:placeholderapi:2.11.3")
