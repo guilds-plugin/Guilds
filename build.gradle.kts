25779f36752579caf24921a51f84481fde4da854
Glare <admin@glaremasters.me>
2023-10-02 09:28:09 -0500

    chore(deps): add support for 1.20.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ import java.net.URL
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.8.22"
-    id("net.kyori.indra") version "3.1.1"
-    id("net.kyori.indra.publishing") version "3.1.1"
-    id("net.kyori.indra.license-header") version "3.1.1"
+    id("org.jetbrains.kotlin.jvm") version "1.9.10"
+    id("net.kyori.indra") version "3.1.3"
+    id("net.kyori.indra.publishing") version "3.1.3"
+    id("net.kyori.indra.license-header") version "3.1.3"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.github.slimjar") version "1.3.0"
     id("xyz.jpenilla.run-paper") version "2.1.0"
-    id("com.github.ben-manes.versions") version "0.47.0"
-    id("org.jetbrains.dokka") version "1.8.20"
+    id("com.github.ben-manes.versions") version "0.48.0"
+    id("org.jetbrains.dokka") version "1.9.0"
 }
 
 group = "me.glaremasters"
@@ -33,7 +33,10 @@ apply {
 repositories {
     mavenCentral()
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
-        content { includeGroup("org.bukkit") }
+        content {
+            includeGroup("org.bukkit")
+            includeGroup("org.spigotmc")
+        }
     }
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://repo.aikar.co/content/groups/aikar/") {
@@ -55,18 +58,18 @@ dependencies {
     implementation("org.codemc.worldguardwrapper:worldguardwrapper:1.1.9-SNAPSHOT")
     implementation("ch.jalu:configme:1.3.0")
     implementation("com.dumptruckman.minecraft:JsonConfiguration:1.1")
-    implementation("com.github.cryptomorin:XSeries:9.4.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.3.0")
+    implementation("com.github.cryptomorin:XSeries:9.6.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.3.1")
     implementation("dev.triumphteam:triumph-gui:3.1.5")
     implementation("com.zaxxer:HikariCP:4.0.3")
     implementation("org.jdbi:jdbi3-core:3.8.2")
     implementation("org.jdbi:jdbi3-sqlobject:3.8.2")
     implementation("org.mariadb.jdbc:mariadb-java-client:2.7.2")
 
-    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT")
     compileOnly("net.milkbowl:vault:1.7")
     compileOnly("com.mojang:authlib:1.5.25")
-    compileOnly("me.clip:placeholderapi:2.11.3")
+    compileOnly("me.clip:placeholderapi:2.11.4")
 
     slim("org.jetbrains.kotlin:kotlin-stdlib")
 }
