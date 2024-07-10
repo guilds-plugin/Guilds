2956232b2e7be26a8704cdddf7574b8f2daab032
Glare <admin@glaremasters.me>
2024-07-10 16:00:27 -0500

    fix: fallback to default url if a skull's texture is broken

--- src/main/kotlin/me/glaremasters/guilds/guis/ListGUI.kt
+++ src/main/kotlin/me/glaremasters/guilds/guis/ListGUI.kt
@@ -101,8 +101,8 @@ class ListGUI(private val guilds: Guilds, private val settingsManager: SettingsM
         val defaultUrl = settingsManager.getProperty(GuildListSettings.GUILD_LIST_HEAD_DEFAULT_URL)
         val useDefaultUrl = settingsManager.getProperty(GuildListSettings.USE_DEFAULT_TEXTURE)
 
-        val item = if (!useDefaultUrl && guild.skull != null && guild.guildSkull != null) {
-            guild.skull
+        val item = if (!useDefaultUrl && guild.guildSkull != null) {
+            guild.guildSkull?.createSkull() ?: GuildSkull(defaultUrl).itemStack
         } else {
             GuildSkull(defaultUrl).itemStack
         }
